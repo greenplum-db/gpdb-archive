fe14a8fe7950fdec048d6ccec797e29a4348a9d1
QingMa <maqi@vmware.com>
2022-02-10 17:00:20 +0800

    Add isolation test for pg_basebackup. Test pg_basebackup with database oid larger than int32 for https://github.com/postgres/postgres/commit/259bbe177808986e5d226ea7ce5a1ebb74657791.

--- src/backend/access/transam/varsup.c
+++ src/backend/access/transam/varsup.c
@@ -608,16 +608,9 @@ GetNewObjectIdUnderLock(void)
 		/*
 		 * CDB: we encounter high oid issues several times, we should
 		 * have some test-utils to verify logic under larger oid.
-		 *
-		 * NOTE: we do not have undo-bump, so take care when you decide to
-		 * use this fault inject. Currently, only resgroup test job uses it,
-		 * that is safe, becase resgroup job is an independent pipeline job.
 		 */
-		Oid large_oid = (1U<<31)+5; /* this value will overflow if taken as int32 */
-		if (ShmemVariableCache->nextOid < large_oid)
-		{
-			ShmemVariableCache->nextOid = large_oid + 1;
-			result = large_oid;
+		if (result <= PG_INT32_MAX) {
+			result = PG_INT32_MAX + result % (PG_UINT32_MAX - PG_INT32_MAX) + 1;
 		}
 	}
 #endif
