dd7898932f61e35e966d3d3883dee8e2f252d805
Jimmy Yih <jyih@vmware.com>
2022-03-04 16:04:33 -0800

    Fix flaky fts_unblock_primary isolation2 test
    The fts_unblock_primary isolation2 test would sometimes fail stating
    that the GUC value was not updated after running `gpstop -u` to reload
    from the updated postgresql.conf file. The cause of the flakiness was
    due to the assumption that the SHOW command would immediately be
    usable after `gpstop -u` returned. The `gpstop -u` command merely
    sends SIGHUP signals to all the segments (e.g. pg_ctl reload on all
    the segments) and does not do any validations. In this test, the SHOW
    command was being run before the SIGHUP signal was handled which
    resulted in the incorrect value being displayed. To fix this test
    flake, we simply need to close our SQL connection after sending the
    SIGHUP signals and open a new connection to run the SHOW command. This
    solution works because the new connection will block until the config
    reload has completed.

--- src/test/isolation2/sql/segwalrep/fts_unblock_primary.sql
+++ src/test/isolation2/sql/segwalrep/fts_unblock_primary.sql
@@ -42,10 +42,11 @@ select gp_inject_fault('fts_probe', 'reset', 1);
 select gp_request_fts_probe_scan();
 select content, role, preferred_role, mode, status from gp_segment_configuration where content=2;
 
--- set mirror down grace period to zero to instantly mark mirror down
+-- set mirror down grace period to zero to instantly mark mirror down.
+-- the 2Uq and 2U pair will force a wait on the config reload.
 !\retcode gpconfig -c gp_fts_mark_mirror_down_grace_period -v 0;
 !\retcode gpstop -u;
-
+2Uq:
 2U: show gp_fts_mark_mirror_down_grace_period;
 
 -- trigger fts probe and check to see primary marked n/u and mirror n/d
@@ -84,6 +85,9 @@ insert into fts_unblock_primary select i from generate_series(1,10)i;
 -- synchronous_standby_names should be back to its original value on the primary
 2U: show synchronous_standby_names;
 
+-- reset the mirror down grace period back to its default value.
+-- the 2Uq and 2U pair will force a wait on the config reload.
 !\retcode gpconfig -r gp_fts_mark_mirror_down_grace_period;
 !\retcode gpstop -u;
+2Uq:
 2U: show gp_fts_mark_mirror_down_grace_period;
