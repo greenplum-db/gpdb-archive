b6f43a0ccffb8b9e7096c76a47d7a341a4f93649
hughcapet <bungina@arenadata.io>
2022-03-23 09:18:45 -0700

    Remove platform specific limitation from msg type recognition logic
    This commit introduces fixes in the functions that work with 'firstchar'
    byte value received from socket.This is done for consistency across the
    codebase and avoidance of misreads of -1 (EOF) value in case gp is
    compiled with default char unsigned (it can mistakenly be treated as
    255 then). An example of such platform: PowerPC.

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -240,7 +240,7 @@ static int	InteractiveBackend(StringInfo inBuf);
 static int	interactive_getc(void);
 static int	SocketBackend(StringInfo inBuf);
 static int	ReadCommand(StringInfo inBuf);
-static void forbidden_in_wal_sender(char firstchar);
+static void forbidden_in_wal_sender(int firstchar);
 static List *pg_rewrite_query(Query *query);
 static bool check_log_statement(List *stmt_list);
 static int	errdetail_execute(List *raw_parsetree_list);
@@ -4633,7 +4633,7 @@ process_postgres_switches(int argc, char *argv[], GucContext ctx,
  * received, and is used to construct the error message.
  */
 static void
-check_forbidden_in_gpdb_handlers(char firstchar)
+check_forbidden_in_gpdb_handlers(int firstchar)
 {
 	if (am_ftshandler || am_faulthandler)
 	{
@@ -4653,7 +4653,7 @@ check_forbidden_in_gpdb_handlers(char firstchar)
 }
 
 static void
-forbidden_in_retrieve_handler(char firstchar)
+forbidden_in_retrieve_handler(int firstchar)
 {
 	if (am_cursor_retrieve_handler)
 		ereport(ERROR,
@@ -5811,7 +5811,7 @@ PostgresMain(int argc, char *argv[],
  * message was received, and is used to construct the error message.
  */
 static void
-forbidden_in_wal_sender(char firstchar)
+forbidden_in_wal_sender(int firstchar)
 {
 	if (am_walsender)
 	{
