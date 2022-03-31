baace3f098febe66f318179856651edf9abe0989
xuejing zhao <80750564+zxuejing@users.noreply.github.com>
2022-03-31 17:52:45 +0800

    resolve update distributed key with oids fixme (#13343)
    In the older version we can create table with (oids=true);
    therefore, each row has an OID, when we update distributed columns of a table,
    a row may move from one segment to another one. so we must ensure that the
    target list contains the old OID so that the Split Update can copy it to the new tuple.
    
    but the newest version of GPDB does not support create table with (oids=true).
    when merging postgres12, the logic of codes is removed by this commit:
    https://github.com/greenplum-db/gpdb-postgres-merge/commit/7c3bd14c32a746c8f8a3e59898d8e587638054fb
    But the comments remain. Now we remove related comments also.
    
    we just modify comments, so there is no test case.

--- src/backend/optimizer/prep/preptlist.c
+++ src/backend/optimizer/prep/preptlist.c
@@ -433,16 +433,7 @@ expand_targetlist(PlannerInfo *root, List *tlist, int command_type,
 	/*
 	 * If an UPDATE can move the tuples from one segment to another, we will
 	 * need to create a Split Update node for it. The node is created later
-	 * in the planning, but if it's needed, and the table has OIDs, we must
-	 * ensure that the target list contains the old OID so that the Split
-	 * Update can copy it to the new tuple.
-	 *
-	 * GPDB_96_MERGE_FIXME: we used to copy all old distribution key columns,
-	 * but we only need this for the OID now. Can we desupport Split Updates
-	 * on tables with OIDs, and get rid of this?
-	 *
-	 * GPDB_12_MERGE_FIXME: Tables with special OIDS is now gone. We can
-	 * definitely get rid of this now.
+	 * in the planning.
 	 */
 	if (command_type == CMD_UPDATE)
 	{
