43be48fd4c9402ad182da19627bcb90a5246fab3
roicos <kdontsova@vmware.com>
2022-04-13 14:26:23 -0700

    Add GPDB PITR tests to GPDB CI
     - This is a new container test which is separate from ICW
       because putting it in the ICW would cause unnecessary backups
       of the entire ICW workload.

---
+++ concourse/scripts/gpdb_pitr.bash
@@ -0,0 +1,67 @@
+#!/bin/bash -l
+
+set -eox pipefail
+
+CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${CWDIR}/common.bash"
+
+function gen_env(){
+  cat > /opt/run_test.sh <<-EOF
+		trap look4diffs ERR
+
+		function look4diffs() {
+
+		    diff_files=\`find .. -name regression.diffs\`
+
+		    for diff_file in \${diff_files}; do
+			if [ -f "\${diff_file}" ]; then
+			    cat <<-FEOF
+
+						======================================================================
+						DIFF FILE: \${diff_file}
+						----------------------------------------------------------------------
+
+						\$(cat "\${diff_file}")
+
+					FEOF
+			fi
+		    done
+		    exit 1
+		}
+		source /usr/local/greenplum-db-devel/greenplum_path.sh
+		cd "\${1}/gpdb_src"
+		source gpAux/gpdemo/gpdemo-env.sh
+		cd "\${1}/gpdb_src/src/test/gpdb_pitr"
+		make installcheck
+	EOF
+
+	chmod a+x /opt/run_test.sh
+}
+
+function setup_gpadmin_user() {
+    ./gpdb_src/concourse/scripts/setup_gpadmin_user.bash "$TEST_OS"
+}
+
+function _main() {
+    if [ -z "$TEST_OS" ]; then
+        echo "FATAL: TEST_OS is not set"
+        exit 1
+    fi
+
+    case "${TEST_OS}" in
+    centos|ubuntu|sles) ;; #Valid
+    *)
+      echo "FATAL: TEST_OS is set to an invalid value: $TEST_OS"
+      echo "Configure TEST_OS to be centos, or ubuntu"
+      exit 1
+      ;;
+    esac
+
+    time install_and_configure_gpdb
+    time setup_gpadmin_user
+    time make_cluster
+    time gen_env
+    time run_test
+}
+
+_main "$@"
