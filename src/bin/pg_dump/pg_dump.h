7281b1247539c2cff1610d7c45851710fc34cefc
Soumyadeep Chakraborty <soumyadeep2007@gmail.com>
2022-07-26 14:34:21 -0700

    pg_dump: Avoid compiler warning in getTypes
    Warning:
    
    pg_dump.c:5467:21: warning: assignment discards ‘const’ qualifier from pointer target type [-Wdiscarded-qualifiers]
     5467 |   tyinfo[i].rolname = getRoleName(PQgetvalue(res, i, i_typowner));
          |                     ^
    
    This was due to a minor mismerge in 1b64faef426.
    Also get rid of the obvious comment, to make it look like upstream.

--- src/bin/pg_dump/pg_dump.h
+++ src/bin/pg_dump/pg_dump.h
@@ -209,7 +209,7 @@ typedef struct _typeInfo
 	 * schema-qualified too.
 	 */
 	char		*ftypname;
-	char		*rolname;		/* name of owner, or empty string */
+	const char  *rolname;
 	char		*typacl;
 	char		*rtypacl;
 	char		*inittypacl;
