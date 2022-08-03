d2559fec2cc2923c9c5a0d18a1a0eff238cc6820
Sasasu <i@sasa.su>
2022-08-03 11:33:13 +0800

    Add testcase for FIPS mode operations in pgcrypto
    ports from 5X 923ecc088d04f851c9f3a52fc26dfa00ac3ef314
    
    Tests pgcrypto function calls using FIPS mode on FIPS mode capable or
    non-capable systems that are compiled against FIPS mode capable or
    non-capable OpenSSL.
    
    Note that FIPS mode requires setting custom GUC pgcrypto.fips at the
    database level. This is problematic during upgrade because the new
    cluster has no way to know about this custom GUC when it's recreating
    the old DDL schemas. For now, the solution for a customer using FIPS is
    to erase the custom GUC during upgrade. This can be done by updating
    pg_database:datconfig as seen in test_gpdb_pre.sql.
    
    Co-authored-by: Kris Macoskey <kmacoskey@pivotal.io>
    Co-authored-by: David Kimura <dkimura@vmware.com>

---
+++ contrib/pgcrypto/sql/fips.sql
@@ -0,0 +1,40 @@
+ALTER DATABASE contrib_regression SET pgcrypto.fips TO on;
+\c contrib_regression
+
+SHOW pgcrypto.fips;
+
+CREATE TABLE fipstest (data text, res text, salt text);
+INSERT INTO fipstest VALUES ('password', '', '');
+
+SELECT 'Test digest md5: EXPECTED ERROR FAIL FIPS' as comment;
+SELECT digest('santa claus', 'md5');
+
+SELECT 'Test digest sha256: EXPECTED PASS' as comment;
+SELECT digest('santa claus', 'sha256');
+
+SELECT 'Test hmac md5: EXPECTED ERROR FAIL FIPS' as comment;
+SELECT hmac('santa claus', 'aaa', 'md5');
+
+SELECT 'Test hmac sha256: EXPECTED PASS' as comment;
+SELECT hmac('santa claus', 'aaa', 'sha256');
+
+SELECT 'Test gen_salt : EXPECTED FAIL FIPS' as comment;
+UPDATE fipstest SET salt = gen_salt('md5');
+
+SELECT 'Test crypt : EXPECTED FAIL FIPS' as comment;
+UPDATE fipstest SET res = crypt(data, salt);
+SELECT res = crypt(data, res) AS "worked" FROM fipstest;
+
+SELECT 'Test pgp : EXPECTED PASS' as comment;
+select pgp_sym_decrypt(pgp_sym_encrypt('santa clause', 'mypass', 'cipher-algo=aes256'), 'mypass');
+
+SELECT 'Test pgp : EXPECTED FAIL FIPS' as comment;
+select pgp_sym_decrypt(pgp_sym_encrypt('santa clause', 'mypass', 'cipher-algo=bf'), 'mypass');
+
+SELECT 'Test raw encrypt : EXPECTED PASS' as comment;
+SELECT encrypt('santa claus', 'mypass', 'aes') as raw_aes;
+
+SELECT 'Test raw encrypt : EXPECTED FAIL FIPS' as comment;
+SELECT encrypt('santa claus', 'mypass', 'bf') as raw_blowfish;
+
+DROP TABLE fipstest;
