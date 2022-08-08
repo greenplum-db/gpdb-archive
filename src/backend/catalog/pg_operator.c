782b169922760d0cf46b1ffd86507e4425e0743c
Tom Lane <tgl@sss.pgh.pa.us>
2022-08-08 11:12:31 -0400

    In extensions, don't replace objects not belonging to the extension.
    Previously, if an extension script did CREATE OR REPLACE and there was
    an existing object not belonging to the extension, it would overwrite
    the object and adopt it into the extension.  This is problematic, first
    because the overwrite is probably unintentional, and second because we
    didn't change the object's ownership.  Thus a hostile user could create
    an object in advance of an expected CREATE EXTENSION command, and would
    then have ownership rights on an extension object, which could be
    modified for trojan-horse-type attacks.
    
    Hence, forbid CREATE OR REPLACE of an existing object unless it already
    belongs to the extension.  (Note that we've always forbidden replacing
    an object that belongs to some other extension; only the behavior for
    previously-free-standing objects changes here.)
    
    For the same reason, also fail CREATE IF NOT EXISTS when there is
    an existing object that doesn't belong to the extension.
    
    Our thanks to Sven Klemm for reporting this problem.
    
    Security: CVE-2022-2625
    
    (cherry picked from commit b9b21acc766db54d8c337d508d0fe2f5bf2daab0)

--- src/backend/catalog/pg_operator.c
+++ src/backend/catalog/pg_operator.c
@@ -874,7 +874,7 @@ makeOperatorDependencies(HeapTuple tuple, bool isUpdate)
 							oper->oprowner);
 
 	/* Dependency on extension */
-	recordDependencyOnCurrentExtension(&myself, true);
+	recordDependencyOnCurrentExtension(&myself, isUpdate);
 
 	return myself;
 }
