d7caa6c99ff0a2928d27111678e0cc83ef85144e
Huansong Fu <fuhuansong@gmail.com>
2022-08-26 15:19:07 -0700

    Resolve fixme comment in ATExecAddColumn() about why no lock child table
    In ATExecAddColumn() where we find child tables to apply a flag,
    we used to open the tables w/o lock. That is because we have already
    held a lock on the parent and we implicitly believed that no one
    else could alter the child tables now. That is still correct, but
    the upstream had introduced an expectation for relation_open()
    in 4b21acf522d751ba5b6679df391d5121b6c4a35f that all callers of
    relation_open() should have acquired a lock except in bootstrap
    mode. I do not think it's important to still use NoLock and try
    to workaround that assert in relation_open() in some way.
    Therefore, resolving the FIXME as it is.

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -7597,17 +7597,12 @@ ATExecAddColumn(List **wqueue, AlteredTableInfo *tab, Relation rel,
 			 * We have acquired lockmode on the root and first-level partitions
 			 * already. This leaves the deeper subpartitions unlocked, but no
 			 * operations can drop (or alter) those relations without locking
-			 * through the root. Note that find_all_inheritors() also includes
-			 * the root partition in the returned list.
-			 *
-			 * GPDB_12_MERGE_FIXME: we used to have NoLock here, but that caused
-			 * assertion failures in the regression tests:
-			 *
-			 * FATAL:  Unexpected internal error (relation.c:74)
-			 * DETAIL:  FailedAssertion("!(lockmode != 0 || (Mode == BootstrapProcessing) || CheckRelationLockedByMe(r, 1, 1))", File: "relation.c", Line: 74)
-			 *
-			 * so use AccessShareLock instead. Was it important that we used
-			 * NoLock here?
+			 * through the root. But we still lock them to meet the upstream 
+			 * expecation in relation_open that all callers should have acquired
+			 * a lock on the table except in bootstrap mode.
+
+			 * Note that find_all_inheritors() also includes the root partition 
+			 * in the returned list.
 			 */
 			List *all_inheritors = find_all_inheritors(tab->relid, AccessShareLock, NULL);
 			ListCell *lc;
