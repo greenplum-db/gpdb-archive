5042934cc808c34abca46e67c806a19e5db68da7
Soumyadeep Chakraborty <soumyadeep2007@gmail.com>
2022-11-08 13:44:32 -0800

    Move tcp_ic_teardown test to dedicated schedules
    It was pointless to run the test in multiple CI boxes (udp, tcp, proxy
    etc). We should run it only in a TCP/proxy environment where TCP
    teardown is applicable.

--- src/test/isolation2/sql/tcp_ic_teardown.sql
+++ src/test/isolation2/sql/tcp_ic_teardown.sql
@@ -1,23 +1,8 @@
 -- Test ensuring that we perform a timed wait inside the TCP interconnect
 -- teardown on the motion sender side, for the final response from the motion
 -- receiver(s).
-
-CREATE FUNCTION set_gp_ic_type(ic_type text)
-    RETURNS VOID as $$
-import os
-cmd = 'gpconfig -c gp_interconnect_type -v %s' % ic_type
-if os.system(cmd) is not 0:
-    plpy.error('Setting gp_interconnect_type to %s failed' % ic_type)
-$$ LANGUAGE plpython3u;
-
 CREATE TABLE tcp_ic_teardown(i int);
 INSERT INTO tcp_ic_teardown SELECT generate_series(1, 5);
-
--- Save current IC type before we set it to 'tcp', so we can revert it at the
--- end of the test.
--1U: CREATE TABLE saved_ic_type AS SELECT current_setting('gp_interconnect_type') AS ic_type;
--1U: SELECT set_gp_ic_type('tcp');
-!\retcode gpstop -au;
 
 SELECT gp_inject_fault('waitOnOutbound', 'suspend', dbid)
     FROM gp_segment_configuration WHERE role = 'p' AND content = 0;
@@ -47,8 +32,3 @@ SELECT gp_inject_fault('doSendStopMessageTCP', 'reset', dbid)
 -- After 6s have elapsed (enough to have covered the timed wait of 3s, we should
 -- have consequently ERRORed out on the motion sender side)
 1<:
-
--- Revert IC type
--1U: SELECT set_gp_ic_type(ic_type) FROM saved_ic_type;
-!\retcode gpstop -au;
--1U: DROP TABLE saved_ic_type;
