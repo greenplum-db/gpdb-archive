a24ed1b1fc52a7a23f47e7bdcdf88ee686ee7a79
Shaoqi Bai <bshaoqi@vmware.com>
2022-11-10 02:46:58 -0800

    Add Rocky8 to platform support for gpdb7
    [GPR-1016]
    
    Co-authored-by: Lucas Bonner <blucas@vmware.com>
    Co-authored-by: Shaoqi Bai <bshaoqi@vmware.com>

--- gpMgmt/test/behave/mgmt_utils/steps/mgmt_utils.py
+++ gpMgmt/test/behave/mgmt_utils/steps/mgmt_utils.py
@@ -46,7 +46,7 @@ def show_all_installed(gphome):
     name = x[0].lower()
     if 'ubuntu' in name:
         return "dpkg --get-selections --admindir=%s/share/packages/database/deb | awk '{print $1}'" % gphome
-    elif 'centos' in name or 'rhel' in name:
+    elif 'centos' in name or 'rhel' in name or 'rocky' in name:
         return "rpm -qa --dbpath %s/share/packages/database" % gphome
     else:
         raise Exception('UNKNOWN platform: %s' % str(x))
@@ -56,7 +56,7 @@ def remove_native_package_command(gphome, full_gppkg_name):
     name = x[0].lower()
     if 'ubuntu' in name:
         return 'fakeroot dpkg --force-not-root --log=/dev/null --instdir=%s --admindir=%s/share/packages/database/deb -r %s' % (gphome, gphome, full_gppkg_name)
-    elif 'centos' in name or 'rhel' in name:
+    elif 'centos' in name or 'rhel' in name or 'rocky' in name:
         return 'rpm -e %s --dbpath %s/share/packages/database' % (full_gppkg_name, gphome)
     else:
         raise Exception('UNKNOWN platform: %s' % str(x))
