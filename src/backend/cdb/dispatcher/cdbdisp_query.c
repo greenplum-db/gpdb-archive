greenplum-db/gpdb-archive

commit 5f1a0bee1c5eadb5966e3554cda72ecaddc3527c

refactor the original cgroup code and abstract the corresponding interface (#14343)
This PR is the second step in refactoring the resource group. The first one is #14256.

In this PR, we do not change any behavior of the resource group, we do not change the
interface exposed to the resource manager, it just abstracts all the fundamental functions
to the struct CGroupOpsRoutine, and use this abstract handler to manipulate the
underlying Linux Cgroup files, there are two purposes for this:

1. make the code more readable.
2. provides the base interface for Linux Cgroup v2.

The second one is our main motivation for doing this.

Of course, this is a relatively large change, so it's not all done, and more details need to be
fixed.

src/backend/cdb/dispatcher/cdbdisp_query.c
@@ -37,7 +37,7 @@
 #include "utils/faultinjector.h"
 #include "utils/resgroup.h"
 #include "utils/resource_manager.h"
-#include "utils/resgroup-ops.h"
+#include "utils/cgroup.h"
 #include "utils/session_state.h"
 #include "utils/typcache.h"
 #include "miscadmin.h"
@@ -279,7 +279,7 @@ CdbDispatchPlan(struct QueryDesc *queryDesc,
 		 * We enable resource group re-calculate the query_mem on QE, and we are not in
 		 * fall back mode (use statement_mem).
 		 */
-		stmt->total_memory_coordinator = ResGroupOps_GetTotalMemory();
+		stmt->total_memory_coordinator = getTotalMemory();
 		stmt->nsegments_coordinator = ResGroupGetHostPrimaryCount();
 	}
 

src/backend/executor/execMain.c
@@ -75,7 +75,7 @@
 #include "utils/workfile_mgr.h"
 #include "utils/faultinjector.h"
 #include "utils/resource_manager.h"
-#include "utils/resgroup-ops.h"
+#include "utils/cgroup.h"
 
 #include "catalog/pg_statistic.h"
 #include "catalog/pg_class.h"
@@ -275,7 +275,7 @@ standard_ExecutorStart(QueryDesc *queryDesc, int eflags)
 					should_skip_operator_memory_assign = false;
 
 					/* Get total system memory on the QE in MB */
-					int 	total_memory_segment = ResGroupOps_GetTotalMemory();
+					int 	total_memory_segment = getTotalMemory();
 					int 	nsegments_segment = ResGroupGetHostPrimaryCount();
 					uint64	coordinator_query_mem = queryDesc->plannedstmt->query_mem;
 

src/backend/utils/resgroup/Makefile
@@ -16,9 +16,8 @@ override CPPFLAGS := -I$(libpq_srcdir) $(CPPFLAGS)
 OBJS = resgroup.o resgroup_helper.o
 
 ifeq ($(PORTNAME),linux)
-OBJS += resgroup-ops-linux.o
-else
-OBJS += resgroup-ops-dummy.o
+OBJS += cgroup.o
+OBJS += cgroup-ops-linux-v1.o
 endif
 
 include $(top_srcdir)/src/backend/common.mk

commit 98dddcf8f126190b24909d0ddfc3c30a395010c9

fix link problem in macOS and Windows (#14587)
fix link problems in macOS and Windows which was introduced by #14343.

src/backend/cdb/dispatcher/cdbdisp_query.c
@@ -50,6 +50,9 @@
 #include "cdb/cdbcopy.h"
 #include "executor/execUtils.h"
 
+extern CGroupOpsRoutine *cgroupOpsRoutine;
+extern CGroupSystemInfo *cgroupSystemInfo;
+
 #define QUERY_STRING_TRUNCATE_SIZE (1024)
 
 extern bool Test_print_direct_dispatch_info;
@@ -279,7 +282,7 @@ CdbDispatchPlan(struct QueryDesc *queryDesc,
 		 * We enable resource group re-calculate the query_mem on QE, and we are not in
 		 * fall back mode (use statement_mem).
 		 */
-		stmt->total_memory_coordinator = getTotalMemory();
+		stmt->total_memory_coordinator = cgroupOpsRoutine->gettotalmemory();
 		stmt->nsegments_coordinator = ResGroupGetHostPrimaryCount();
 	}
 

src/backend/executor/execMain.c
@@ -109,6 +109,9 @@
 #include "cdb/cdbutil.h"
 #include "cdb/cdbendpoint.h"
 
+extern CGroupOpsRoutine *cgroupOpsRoutine;
+extern CGroupSystemInfo *cgroupSystemInfo;
+
 #define IS_PARALLEL_RETRIEVE_CURSOR(queryDesc)	(queryDesc->ddesc &&	\
 										queryDesc->ddesc->parallelCursorName &&	\
 										strlen(queryDesc->ddesc->parallelCursorName) > 0)
@@ -275,7 +278,7 @@ standard_ExecutorStart(QueryDesc *queryDesc, int eflags)
 					should_skip_operator_memory_assign = false;
 
 					/* Get total system memory on the QE in MB */
-					int 	total_memory_segment = getTotalMemory();
+					int 	total_memory_segment = cgroupOpsRoutine->gettotalmemory();
 					int 	nsegments_segment = ResGroupGetHostPrimaryCount();
 					uint64	coordinator_query_mem = queryDesc->plannedstmt->query_mem;
 

src/backend/utils/resgroup/Makefile
@@ -18,6 +18,8 @@ OBJS = resgroup.o resgroup_helper.o
 ifeq ($(PORTNAME),linux)
 OBJS += cgroup.o
 OBJS += cgroup-ops-linux-v1.o
+else
+OBJS += cgroup-ops-dummy.o
 endif
 
 include $(top_srcdir)/src/backend/common.mk

src/backend/utils/resgroup/cgroup-ops-dummy.c
@@ -1,0 +1,314 @@
+/*-------------------------------------------------------------------------
+ *
+ * cgroup-ops-dummy.c
+ *    OS dependent resource group operations - dummy implementation
+ *
+ * Copyright (c) 2017 VMware, Inc. or its affiliates.
+ *
+ *
+ * IDENTIFICATION
+ *      src/backend/utils/resgroup/cgroup-ops-dummy.c
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "postgres.h"
+
+#include "utils/cgroup.h"
+#include "utils/cgroup-ops-dummy.h"
+
+/*
+ * Interfaces for OS dependent operations.
+ *
+ * Resource group relies on OS dependent group implementation to manage
+ * resources like cpu usage, such as cgroup on Linux system.
+ * We call it OS group in below function description.
+ *
+ * So far these operations are mainly for CPU rate limitation and accounting.
+ */
+
+#define unsupported_system() \
+    elog(WARNING, "resource group is not supported on this system")
+
+
+static CGroupSystemInfo cgroupSystemInfoAlpha = {
+		0,
+		""
+};
+
+/* Return the name for the OS group implementation */
+static const char *
+getcgroupname_dummy(void)
+{
+	return "unsupported";
+}
+
+/*
+ * Probe the configuration for the OS group implementation.
+ *
+ * Return true if everything is OK, or false is some requirements are not
+ * satisfied.  Will not fail in either case.
+ */
+static bool
+probecgroup_dummy(void)
+{
+	return false;
+}
+
+/* Check whether the OS group implementation is available and usable */
+static void
+checkcgroup_dummy(void)
+{
+	unsupported_system();
+}
+
+/* Initialize the OS group */
+static void
+initcgroup_dummy(void)
+{
+	unsupported_system();
+}
+
+/* Adjust GUCs for this OS group implementation */
+static void
+adjustgucs_dummy(void)
+{
+	unsupported_system();
+}
+
+/*
+ * Create the OS group for group.
+ */
+static void
+createcgroup_dummy(Oid group)
+{
+	unsupported_system();
+}
+
+/*
+ * Assign a process to the OS group. A process can only be assigned to one
+ * OS group, if it's already running under other OS group then it'll be moved
+ * out that OS group.
+ *
+ * pid is the process id.
+ */
+static void
+attachcgroup_dummy(Oid group, int pid, bool is_cpuset_enabled)
+{
+	unsupported_system();
+}
+
+/*
+ * un-assign all the processes from a cgroup.
+ *
+ * These processes will be moved to the gpdb default cgroup.
+ *
+ * This function must be called with the gpdb toplevel dir locked,
+ * fd_dir is the fd for this lock, on any failure fd_dir will be closed
+ * (and unlocked implicitly) then an error is raised.
+ */
+static void
+detachcgroup_dummy(Oid group, CGroupComponentType component, int fd_dir)
+{
+	unsupported_system();
+}
+
+/*
+ * Destroy the OS group for group.
+ *
+ * One OS group can not be dropped if there are processes running under it,
+ * if migrate is true these processes will be moved out automatically.
+ */
+static void
+destroycgroup_dummy(Oid group, bool migrate)
+{
+	unsupported_system();
+}
+
+/*
+ * Lock the OS group. While the group is locked it won't be removed by other
+ * processes.
+ *
+ * This function would block if block is true, otherwise it returns with -1
+ * immediately.
+ *
+ * On success, it returns a fd to the OS group, pass it to
+ * ResGroupOps_UnLockGroup() to unlock it.
+ */
+static int
+lockcgroup_dummy(Oid group, CGroupComponentType component, bool block)
+{
+	unsupported_system();
+	return -1;
+}
+
+/*
+ * Unblock an OS group.
+ *
+ * fd is the value returned by lockcgroup_dummy().
+ */
+static void
+unlockcgroup_dummy(int fd)
+{
+	unsupported_system();
+}
+
+/*
+ * Set the cpu rate limit for the OS group.
+ *
+ * cpu_rate_limit should be within [0, 100].
+ */
+static void
+setcpulimit_dummy(Oid group, int cpu_rate_limit)
+{
+	unsupported_system();
+}
+
+/*
+ * Set the memory limit for the OS group by value.
+ *
+ * memory_limit is the limit value in chunks
+ *
+ * If cgroup supports memory swap, we will write the same limit to
+ * memory.memsw.limit and memory.limit.
+ */
+static void
+setmemorylimitbychunks_dummy(Oid group, int32 memory_limit_chunks)
+{
+	unsupported_system();
+}
+
+/*
+ * Set the memory limit for the OS group by rate.
+ *
+ * memory_limit should be within [0, 100].
+ */
+static void
+setmemorylimit_dummy(Oid group, int memory_limit)
+{
+	unsupported_system();
+}
+
+/*
+ * Get the cpu usage of the OS group, that is the total cpu time obtained
+ * by this OS group, in nano seconds.
+ */
+static int64
+getcpuusage_dummy(Oid group)
+{
+	unsupported_system();
+	return 0;
+}
+
+/*
+ * Get the memory usage of the OS group
+ *
+ * memory usage is returned in chunks
+ */
+static int32
+getmemoryusage_dummy(Oid group)
+{
+	unsupported_system();
+	return 0;
+}
+
+/*
+ * Get the memory limit of the OS group
+ *
+ * memory limit is returned in chunks
+ */
+static int32
+getmemorylimitchunks_dummy(Oid group)
+{
+	unsupported_system();
+	return 0;
+}
+
+/*
+ * Get the cpuset of the OS group.
+ * @param group: the destination group
+ * @param cpuset: the str to be set
+ * @param len: the upper limit of the str
+ */
+static void
+getcpuset_dummy(Oid group, char *cpuset, int len)
+{
+	unsupported_system();
+}
+
+/*
+ * Set the cpuset for the OS group.
+ * @param group: the destination group
+ * @param cpuset: the value to be set
+ * The syntax of CPUSET is a combination of the tuples, each tuple represents
+ * one core number or the core numbers interval, separated by comma.
+ * E.g. 0,1,2-3.
+ */
+static void
+setcpuset_dummy(Oid group, const char *cpuset)
+{
+	unsupported_system();
+}
+
+/*
+ * Convert the cpu usage to percentage within the duration.
+ *
+ * usage is the delta of GetCpuUsage() of a duration,
+ * duration is in micro seconds.
+ *
+ * When fully consuming one cpu core the return value will be 100.0 .
+ */
+static float
+convertcpuusage_dummy(int64 usage, int64 duration)
+{
+	unsupported_system();
+	return 0.0;
+}
+
+static int32
+gettotalmemory_dummy(void)
+{
+	unsupported_system();
+	return 0.0;
+}
+
+static CGroupOpsRoutine cGroupOpsRoutineDummy = {
+		.getcgroupname = getcgroupname_dummy,
+
+		.probecgroup = probecgroup_dummy,
+		.checkcgroup = checkcgroup_dummy,
+		.initcgroup = initcgroup_dummy,
+		.adjustgucs = adjustgucs_dummy,
+		.createcgroup = createcgroup_dummy,
+		.destroycgroup = destroycgroup_dummy,
+
+		.attachcgroup = attachcgroup_dummy,
+		.detachcgroup = detachcgroup_dummy,
+
+		.lockcgroup = lockcgroup_dummy,
+		.unlockcgroup = unlockcgroup_dummy,
+
+		.setcpulimit = setcpulimit_dummy,
+		.getcpuusage = getcpuusage_dummy,
+
+		.gettotalmemory = gettotalmemory_dummy,
+		.getmemoryusage = getmemoryusage_dummy,
+		.setmemorylimit = setmemorylimit_dummy,
+		.getmemorylimitchunks = getmemorylimitchunks_dummy,
+		.setmemorylimitbychunks = setmemorylimitbychunks_dummy,
+
+		.getcpuset = getcpuset_dummy,
+		.setcpuset = setcpuset_dummy,
+
+		.convertcpuusage = convertcpuusage_dummy,
+};
+
+CGroupOpsRoutine *get_cgroup_routine_dummy(void)
+{
+	return &cGroupOpsRoutineDummy;
+}
+
+CGroupSystemInfo *get_cgroup_sysinfo_dummy(void)
+{
+	return &cgroupSystemInfoAlpha;
+}

src/backend/utils/resgroup/cgroup-ops-linux-v1.c
@@ -1244,6 +1244,86 @@ getcpuusage_v1(Oid group)
 	return readInt64(group, BASEDIR_GPDB, component, "cpuacct.usage");
 }
 
+/* get cgroup ram and swap (in Byte) */
+static void
+get_cgroup_memory_info(uint64 *cgram, uint64 *cgmemsw)
+{
+	CGroupComponentType component = CGROUP_COMPONENT_MEMORY;
+
+	*cgram = readInt64(CGROUP_ROOT_ID, BASEDIR_PARENT,
+					   component, "memory.limit_in_bytes");
+
+	if (gp_resource_group_enable_cgroup_swap)
+	{
+		*cgmemsw = readInt64(CGROUP_ROOT_ID, BASEDIR_PARENT,
+							 component, "memory.memsw.limit_in_bytes");
+	}
+	else
+	{
+		elog(DEBUG1, "swap memory is unlimited");
+		*cgmemsw = (uint64) -1LL;
+	}
+}
+
+/* get total ram and total swap (in Byte) from sysinfo */
+static void
+get_memory_info(unsigned long *ram, unsigned long *swap)
+{
+	struct sysinfo info;
+	if (sysinfo(&info) < 0)
+		elog(ERROR, "can't get memory information: %m");
+	*ram = info.totalram;
+	*swap = info.totalswap;
+}
+
+/* get vm.overcommit_ratio */
+static int
+getOvercommitRatio(void)
+{
+	int ratio;
+	char data[MAX_INT_STRING_LEN];
+	size_t datasize = sizeof(data);
+	const char *path = "/proc/sys/vm/overcommit_ratio";
+
+	readData(path, data, datasize);
+
+	if (sscanf(data, "%d", &ratio) != 1)
+		elog(ERROR, "invalid number '%s' in '%s'", data, path);
+
+	return ratio;
+}
+
+static int
+gettotalmemory_v1(void)
+{
+	unsigned long ram, swap, total;
+	int overcommitRatio;
+	uint64 cgram, cgmemsw;
+	uint64 memsw;
+	uint64 outTotal;
+
+	overcommitRatio = getOvercommitRatio();
+	get_memory_info(&ram, &swap);
+	/* Get sysinfo total ram and swap size. */
+	memsw = ram + swap;
+	outTotal = swap + ram * overcommitRatio / 100;
+	get_cgroup_memory_info(&cgram, &cgmemsw);
+	ram = Min(ram, cgram);
+	/*
+	 * In the case that total ram and swap read from sysinfo is larger than
+	 * from cgroup, ram and swap must both be limited, otherwise swap must
+	 * not be limited(we can safely use the value from sysinfo as swap size).
+	 */
+	if (cgmemsw < memsw)
+		swap = cgmemsw - ram;
+	/*
+	 * If it is in container, the total memory is limited by both the total
+	 * memoery outside and the memsw of the container.
+	 */
+	total = Min(outTotal, swap + ram);
+	return total >> BITS_IN_MB;
+}
+
 /*
  * Get the memory usage of the OS group
  *
@@ -1401,6 +1481,7 @@ static CGroupOpsRoutine cGroupOpsRoutineAlpha = {
 		.getcpuset = getcpuset_v1,
 		.setcpuset = setcpuset_v1,
 
+		.gettotalmemory = gettotalmemory_v1,
 		.getmemoryusage = getmemoryusage_v1,
 		.setmemorylimit = setmemorylimit_v1,
 		.getmemorylimitchunks = getmemorylimitchunks_v1,

src/backend/utils/resgroup/cgroup.c
@@ -578,83 +578,3 @@ getCgroupMountDir()
 
 	return strlen(cgroupSystemInfo->cgroup_dir) != 0;
 }
-
-/* get vm.overcommit_ratio */
-static int
-getOvercommitRatio(void)
-{
-	int ratio;
-	char data[MAX_INT_STRING_LEN];
-	size_t datasize = sizeof(data);
-	const char *path = "/proc/sys/vm/overcommit_ratio";
-
-	readData(path, data, datasize);
-
-	if (sscanf(data, "%d", &ratio) != 1)
-		elog(ERROR, "invalid number '%s' in '%s'", data, path);
-
-	return ratio;
-}
-
-/* get cgroup ram and swap (in Byte) */
-static void
-getCgMemoryInfo(uint64 *cgram, uint64 *cgmemsw)
-{
-	CGroupComponentType component = CGROUP_COMPONENT_MEMORY;
-
-	*cgram = readInt64(CGROUP_ROOT_ID, BASEDIR_PARENT,
-					   component, "memory.limit_in_bytes");
-
-	if (gp_resource_group_enable_cgroup_swap)
-	{
-		*cgmemsw = readInt64(CGROUP_ROOT_ID, BASEDIR_PARENT,
-							 component, "memory.memsw.limit_in_bytes");
-	}
-	else
-	{
-		elog(DEBUG1, "swap memory is unlimited");
-		*cgmemsw = (uint64) -1LL;
-	}
-}
-
-/* get total ram and total swap (in Byte) from sysinfo */
-static void
-getMemoryInfo(unsigned long *ram, unsigned long *swap)
-{
-	struct sysinfo info;
-	if (sysinfo(&info) < 0)
-		elog(ERROR, "can't get memory information: %m");
-	*ram = info.totalram;
-	*swap = info.totalswap;
-}
-
-int
-getTotalMemory(void)
-{
-	unsigned long ram, swap, total;
-	int overcommitRatio;
-	uint64 cgram, cgmemsw;
-	uint64 memsw;
-	uint64 outTotal;
-
-	overcommitRatio = getOvercommitRatio();
-	getMemoryInfo(&ram, &swap);
-	/* Get sysinfo total ram and swap size. */
-	memsw = ram + swap;
-	outTotal = swap + ram * overcommitRatio / 100;
-	getCgMemoryInfo(&cgram, &cgmemsw);
-	ram = Min(ram, cgram);
-	/*
-	 * In the case that total ram and swap read from sysinfo is larger than
-	 * from cgroup, ram and swap must both be limited, otherwise swap must
-	 * not be limited(we can safely use the value from sysinfo as swap size).
-	 */
-	if (cgmemsw < memsw)
-		swap = cgmemsw - ram;
-	/*
-	 * If it is in container, the total memory is limited by both the total
-	 * memoery outside and the memsw of the container.
-	 */
-	total = Min(outTotal, swap + ram);
-	return total >> BITS_IN_MB;
-}

src/backend/utils/resgroup/resgroup.c
@@ -74,6 +74,10 @@
 #include "utils/session_state.h"
 #include "utils/vmem_tracker.h"
 #include "utils/cgroup-ops-v1.h"
+#include "utils/cgroup-ops-dummy.h"
+
+extern CGroupOpsRoutine *cgroupOpsRoutine;
+extern CGroupSystemInfo *cgroupSystemInfo;
 
 #define InvalidSlotId	(-1)
 #define RESGROUP_MAX_SLOTS	(MaxConnections)
@@ -548,7 +552,8 @@ initCgroup(void)
 		cgroupSystemInfo = get_cgroup_sysinfo_alpha();
 	}
 #else
-	elog(ERROR, "The resource group is not support on your operating system.");
+	cgroupOpsRoutine = get_cgroup_routine_dummy();
+	cgroupSystemInfo = get_cgroup_sysinfo_dummy();
 #endif
 
 	bool probe_result = cgroupOpsRoutine->probecgroup();
@@ -2144,7 +2149,7 @@ decideTotalChunks(int32 *totalChunks, int32 *chunkSizeInBits)
 	nsegments = Gp_role == GP_ROLE_EXECUTE ? host_primary_segment_count : pResGroupControl->segmentsOnMaster;
 	Assert(nsegments > 0);
 
-	tmptotalChunks = getTotalMemory() * gp_resource_group_memory_limit / nsegments;
+	tmptotalChunks = cgroupOpsRoutine->gettotalmemory() * gp_resource_group_memory_limit / nsegments;
 
 	/*
 	 * If vmem is larger than 16GB (i.e., 16K MB), we make the chunks bigger

src/include/utils/cgroup-ops-dummy.h
@@ -1,0 +1,24 @@
+/*-------------------------------------------------------------------------
+ *
+ * cgroup-ops-dummy.h
+ *	  GPDB resource group definitions.
+ *
+ * Copyright (c) 2017 VMware, Inc. or its affiliates.
+ *
+ *
+ * IDENTIFICATION
+ *	    src/include/utils/cgroup-ops-dummy.h
+ *
+ * This file is for the OS that do not support cgroup, such as Windows, MacOS.
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef RES_GROUP_OPS_DUMMY_H
+#define RES_GROUP_OPS_DUMMY_H
+
+#include "utils/cgroup.h"
+
+extern CGroupOpsRoutine *get_cgroup_routine_dummy(void);
+extern CGroupSystemInfo *get_cgroup_sysinfo_dummy(void);
+
+#endif   /* RES_GROUP_OPS_DUMMY_H */

src/include/utils/cgroup.h
@@ -132,7 +132,6 @@ extern bool deleteDir(Oid group, CGroupComponentType component, const char *file
 
 extern int getCPUCores(void);
 extern bool getCgroupMountDir(void);
-extern int getTotalMemory(void);
 
 /*
  * Interfaces for OS dependent operations
@@ -174,6 +173,7 @@ typedef void (*setcpushare_function) (Oid group, int cpu_share);
 /* Get the cpu usage of the OS group. */
 typedef int64 (*getcpuusage_function) (Oid group);
 
+typedef int32 (*gettotalmemory_function) (void);
 typedef int32 (*getmemoryusage_function) (Oid group);
 typedef int32 (*getmemorylimitchunks_function) (Oid group);
 typedef void (*setmemorylimit_function) (Oid group, int memory_limit);
@@ -215,6 +215,7 @@ typedef struct CGroupOpsRoutine
 
 	getcpuusage_function 	getcpuusage;
 
+	gettotalmemory_function gettotalmemory;
 	getmemoryusage_function getmemoryusage;
 	setmemorylimit_function setmemorylimit;
 	getmemorylimitchunks_function getmemorylimitchunks;
@@ -227,9 +228,9 @@ typedef struct CGroupOpsRoutine
 } CGroupOpsRoutine;
 
 /* The global function handler. */
-extern CGroupOpsRoutine *cgroupOpsRoutine;
+CGroupOpsRoutine *cgroupOpsRoutine;
 
 /* The global system info. */
-extern CGroupSystemInfo *cgroupSystemInfo;
+CGroupSystemInfo *cgroupSystemInfo;
 
 #endif	/* CGROUP_H */