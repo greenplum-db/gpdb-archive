23c5acf8c8ccc26f63596728fd7898bee41dc6e7
Adam Lee <adlee@vmware.com>
2022-12-14 11:35:24 +0800

    Speed up formatter_fixedwidth's test cases (#14639)
    readable_query32 and readable_query33 take ~500 seconds without this
    commit and less than 1 second with it.
    
    1, don't `echo | awk`, just use `printf` to generate the dataset.
    
    ```
    $ time for x in {1..50000};do echo `echo $x | awk '{printf("%010d",$0)}'`; done
    ...
    real	2m43.169s
    user	2m10.596s
    sys	1m7.696s
    ```
    
    ```
    $ time for x in {1..50000};do printf "%010d\n" $x; done
    ...
    real	0m0.314s
    user	0m0.159s
    sys	0m0.155s
    ```
    
    2, reduce the dataset size to one-fifth.

--- contrib/formatter_fixedwidth/scripts/single_field_generator.sh
+++ contrib/formatter_fixedwidth/scripts/single_field_generator.sh
@@ -5,6 +5,6 @@
 # Generate a single field large file test dataset.
 # It's used by readable_query32 test case and could avoid uploading a large file.
 
-for x in {1..50000};do
-    echo `echo $x | awk '{printf("%010d",$0)}'`;
+for x in {40001..50000};do
+    printf "%010d\n" $x;
 done
