62168650d07f232952d12eaef6a0da68b86da49e
Denis Smirnov <sd@arenadata.io>
2023-01-05 17:54:18 -0800

    Convert float8 with double instead of long double
    It seems that GPDB faced a glibc bug on RedHat 6 with denormalized
    numbers and fixed it using two-step conversion in `float8in()`:
    
    - text to long double
    - long double to double after denormalized numbers validation
    
    This differs from PG approach where we rely on libc and make the
    direct conversion to double. RedHat 6 seems to be an old system and
    there is no much sense to differ from upstream and have additional
    performance penalty converting long double to double in a two-step
    manner and making additional checks.
    
    There is another problem with this code - long double can have
    problems on some architectures that are currently not supported by
    GPDB, but where it can be ported in the future. Originally this
    problem was detected as research about porting GPDB to PowerPC: it
    doesn't have analogue of Intel 80-bit long double with 63-bit mantissa
    and extended range between 1.0E-4932W and 1.0E+4932W.  Instead,
    PowerPC has an extended double type combined from two doubles with
    106-bit mantissa and the same range as double provides 1.0E−323L -
    1.0E+308L. As a result, we can't use the same validation logic for the
    numbers near +/- infinity as it is possible to do on Intel. So this
    GPDB specific code doesn't help us to solve the problem on RedHat 6 on
    PowerPC and fails the float test.
    
    So, let's get rid of it and return the code to PG approach with a
    double. Align float.c to upstream version at commit 9e1c9f till where
    we merged to main branch so far.
    
    Co-authored-by: Ashwin Agrawal <aashwin@vmware.com>
    Reviewed-by: Alexandra Wang <walexandra@vmware.com>

--- src/backend/utils/adt/float.c
+++ src/backend/utils/adt/float.c
@@ -30,8 +30,6 @@
 #include "utils/sortsupport.h"
 #include "utils/timestamp.h"
 
-#include "utils/float_utils.h"
-
 
 /*
  * Configurable GUC parameter
@@ -375,9 +373,8 @@ float8in_internal_opt_error(char *num, char **endptr_p,
 							const char *type_name, const char *orig_string,
 							bool *have_error)
 {
-	long double		val;
+	double		val;
 	char	   *endptr;
-	bool 		literal_inf = true;
 
 	/* skip leading whitespace */
 	while (*num != '\0' && isspace((unsigned char) *num))
@@ -394,7 +391,7 @@ float8in_internal_opt_error(char *num, char **endptr_p,
 									 type_name, orig_string))));
 
 	errno = 0;
-	val = strtold(num, &endptr);
+	val = strtod(num, &endptr);
 
 	/* did we not see anything that looks like a double? */
 	if (endptr == num || errno != 0)
@@ -416,7 +413,7 @@ float8in_internal_opt_error(char *num, char **endptr_p,
 			val = get_float8_nan();
 			endptr = num + 3;
 		}
-		else if (pg_strncasecmp(num, "Infinity", 8) == 0 || pg_strncasecmp(num, "+Infinity", 9) == 0)
+		else if (pg_strncasecmp(num, "Infinity", 8) == 0)
 		{
 			val = get_float8_infinity();
 			endptr = num + 8;
@@ -504,31 +501,6 @@ float8in_internal_opt_error(char *num, char **endptr_p,
 							  errmsg("invalid input syntax for type "
 									 "%s: \"%s\"",
 									 type_name, orig_string))));
-
-	/*
-	 * strtod does not support values from 1e-323 to 1e-308 for double datatype in rhel6
-	 * due to changes in glibc. In order to overcome this issue we use strtold to parse
-	 * the string and store it in long double so that we get higher precision. We check if
-	 * this value is within allowed range for double using the below two checks:- 
-	 *
-	 * 1) Overflow - When we try to store a value > 1e308 in a double datatype, it gets represented 
-	 * internally as "inf". Hence in order to check for overflow, we use the isinf() method in order
-	 * to check if (double) val has overflowed. But "inf" is a legitimate value for float8, hence if the
-	 * user entered "inf"/"Infinity" we allow it. Otherwise we treat it as overflow.
-	 *
-	 * 2) Underflow - When we try to store a value < 1e-323, it gets converted to a 0.0. But "0.0" is a 
-	 * legitimate value allowed by float8 datatype. Hence we examine if the val is > 0 or < 0 with higher 
-	 * precision (as long double). Again, 0.0 is allowed for float8. If the user entered 0.0, val will be 0.0
-	 * in both higher precision and when rounded down and we allow it. Otherwise in higher precision, 
-	 * val will be > 0 or < 0 and when rounded down to double it will be 0 which is treated as underflow.
-	 */
-
-	if ( pg_strncasecmp(num, "Infinity", 8) != 0  && pg_strncasecmp(num, "-Infinity", 9) != 0 &&\
-		 pg_strncasecmp(num, "Inf", 3) != 0 && pg_strncasecmp(num, "-Inf", 4) != 0 &&\
-		 pg_strncasecmp(num, "+Infinity", 9) != 0 && pg_strncasecmp(num, "+Inf", 4) != 0 )
-		literal_inf = false;
-
-	CHECKFLOATVAL((double) val, literal_inf, !(val > 0 || val < 0));
 
 	return val;
 }
@@ -1814,6 +1786,7 @@ dcos(PG_FUNCTION_ARGS)
 	PG_RETURN_FLOAT8(result);
 }
 
+
 /*
  *		dcot			- returns the cotangent of arg1 (radians)
  */
@@ -1866,6 +1839,7 @@ dsin(PG_FUNCTION_ARGS)
 	PG_RETURN_FLOAT8(result);
 }
 
+
 /*
  *		dtan			- returns the tangent of arg1 (radians)
  */
@@ -1890,6 +1864,7 @@ dtan(PG_FUNCTION_ARGS)
 	check_float8_val(result, true /* tan(pi/2) == Inf */ , true);
 	PG_RETURN_FLOAT8(result);
 }
+
 
 /* ========== DEGREE-BASED TRIGONOMETRIC FUNCTIONS ========== */
 
