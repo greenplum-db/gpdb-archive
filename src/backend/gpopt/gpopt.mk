811928fd62ba89c470150096720730dc25b9ead9
Tao Tang <taot@vmware.com>
2023-01-10 23:41:56 +0800

    LLVM bitcode generation for gpopt/gporca/gpcloud disabled
    because:
    
    1) ORCA (gpopt, gporca) - All bitcodes are used by LLVM JIT inlining
    optimization step, which occurs in executor stage. ORCA is only accessed in
    plan stage, so there is no need to build bitcodes;
    
    2) gpcloud - C++ is more complex and related LLVM API seems not so stable.
    The upstream code also DO NOT have C++ module, making C++ bitcode new to us.
    Considering the rare usage of gpcloud and the risk, it would be better
    to disable before we have a full verfication and more C++ modules used in
    executor stage.
    
    3) And a side effect - no workaround needed for emiting bitcode of C++ modules,
    even for CLANG/LLVM version < 9. The config/llvm.m4 keeps synced to supported
    LLVM versions. We also avoid updating the build system to solving CXX standard
    collision between ORCA requirement and the one CLANG provides by default.
    
    To disable bitcodes for the above parts, `with_llvm = no` is set before
    including src/backend/common.mk (for gporca/gpopt) and pgxs.mk (for gpcloud)
    In addition, gporca/gpopt are filered out in install-postgres-bitcode.

--- src/backend/gpopt/gpopt.mk
+++ src/backend/gpopt/gpopt.mk
@@ -2,3 +2,8 @@ override CPPFLAGS := -I$(top_srcdir)/src/backend/gporca/libgpos/include $(CPPFLA
 override CPPFLAGS := -I$(top_srcdir)/src/backend/gporca/libgpopt/include $(CPPFLAGS)
 override CPPFLAGS := -I$(top_srcdir)/src/backend/gporca/libnaucrates/include $(CPPFLAGS)
 override CPPFLAGS := -I$(top_srcdir)/src/backend/gporca/libgpdbcost/include $(CPPFLAGS)
+
+# orca is not accessed in JIT (executor stage), avoid the generation of .bc here
+# NOTE: accordingly we MUST avoid them in install step (install-postgres-bitcode
+# in src/backend/Makefile)
+with_llvm = no
--- src/backend/gporca/gporca.mk
+++ src/backend/gporca/gporca.mk
@@ -5,3 +5,8 @@ override CPPFLAGS := -I$(top_srcdir)/src/backend/gporca/libgpdbcost/include $(CP
 # Do not omit frame pointer. Even with RELEASE builds, it is used for
 # backtracing.
 override CXXFLAGS := -Werror -Wextra -Wpedantic -fno-omit-frame-pointer $(CXXFLAGS)
+
+# orca is not accessed in JIT (executor stage), avoid the generation of .bc here
+# NOTE: accordingly we MUST avoid them in install step (install-postgres-bitcode
+# in src/backend/Makefile)
+with_llvm = no
