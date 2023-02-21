open-gpdb/gpdb

Fix KeepLogSeg() unittest
The current WAL record pointer passed by the test to KeepLogSeg was
older that _logSegNo.  This cannot happen, so fix it by increasing it
beyond _logSegNo.  The test needs to be refactored heavily to make use
of new WAL format, but let's do it as a follow up.

Reviewed by: Ashwin Agrawal <aashwin@vmware.com>
Reviewed by: Paul Guo <paulguo@gmail.com>

src/backend/access/transam/test/xlog_test.c
@@ -118,15 +118,15 @@ test_KeepLogSeg(void **state)
 	 ***********************************************/
 	/* Current Delete pointer */
 	wal_keep_segments = 0;
-	_logSegNo = 9 * XLogSegmentsPerXLogId + 45;
+	_logSegNo = recptr / XLogSegSize - 3;
 
 	KeepLogSeg_wrapper(recptr, &_logSegNo);
-	assert_int_equal(_logSegNo, 9*XLogSegmentsPerXLogId + 45);
+	assert_int_equal(_logSegNo, recptr / XLogSegSize - 3);
 
 	wal_keep_segments = -1;
 
 	KeepLogSeg_wrapper(recptr, &_logSegNo);
-	assert_int_equal(_logSegNo, 9*XLogSegmentsPerXLogId + 45);
+	assert_int_equal(_logSegNo, recptr / XLogSegSize - 3);
 	/************************************************/
 }
 

greenplum-db/gpdb-archive

(cherry picked from commit e4ec851b151267fe3209af6804e438611836a875)

src/backend/access/transam/test/xlog_test.c
@@ -123,15 +123,15 @@ test_KeepLogSeg(void **state)
 	 ***********************************************/
 	/* Current Delete pointer */
 	wal_keep_size_mb = 0;
-	_logSegNo = 9 * XLogSegmentsPerXLogId(wal_segment_size) + 45;
+	_logSegNo = recptr / wal_segment_size - 3;
 
 	KeepLogSeg_wrapper(recptr, &_logSegNo);
-	assert_int_equal(_logSegNo, 9*XLogSegmentsPerXLogId(wal_segment_size) + 45);
+	assert_int_equal(_logSegNo, recptr / wal_segment_size - 3);
 
 	wal_keep_size_mb = -1;
 
 	KeepLogSeg_wrapper(recptr, &_logSegNo);
-	assert_int_equal(_logSegNo, 9*XLogSegmentsPerXLogId(wal_segment_size) + 45);
+	assert_int_equal(_logSegNo, recptr / wal_segment_size - 3);
 	/************************************************/
 }
 