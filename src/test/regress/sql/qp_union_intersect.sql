86c0ef34e432f2ba1f927caded276e680a8b2820
Chris Hajas <chajas@vmware.com>
2023-02-23 09:43:40 -0800

    Orca FIXME: enable previously disabled test
    The wrong results in this test was fixed in ce25faf3c84084de1a58a536bc3b8451932e7e4c

--- src/test/regress/sql/qp_union_intersect.sql
+++ src/test/regress/sql/qp_union_intersect.sql
@@ -267,12 +267,9 @@ SELECT COUNT(*) FROM dml_union_r;
 -- @description union_test29: INSERT NON ATOMICS with union/intersect/except
 begin;
 SELECT COUNT(*) FROM dml_union_r;
--- GPDB_12_MERGE_FIXME: ORCA doesn't produce the right intersect plan
-set optimizer=off;
 SELECT COUNT(*) FROM (SELECT dml_union_r.* FROM dml_union_r INTERSECT (SELECT dml_union_r.* FROM dml_union_r UNION ALL SELECT dml_union_s.* FROM dml_union_s) EXCEPT SELECT dml_union_s.* FROM dml_union_s)foo;
 INSERT INTO dml_union_r SELECT dml_union_r.* FROM dml_union_r INTERSECT (SELECT dml_union_r.* FROM dml_union_r UNION ALL SELECT dml_union_s.* FROM dml_union_s) EXCEPT SELECT dml_union_s.* FROM dml_union_s;
 SELECT COUNT(*) FROM dml_union_r;
-reset optimizer;
 rollback;
 
 -- @description union_test30: INSERT NON ATOMICS with union/intersect/except
