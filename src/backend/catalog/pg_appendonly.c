haolinw/gpdb

Introduce "version" attribute to pg_appendonly.
This attribute is used to align with "formatversion" of
pg_aoseg catalog for identifying the append-optimized relation
version during planning stage.

This will simplify validation for version dependent features
such like index only scan, unique index creation and so on.

src/backend/catalog/pg_appendonly.c
@@ -51,7 +51,8 @@ InsertAppendOnlyEntry(Oid relid,
 					  Oid blkdirrelid,
 					  Oid blkdiridxid,
 					  Oid visimaprelid,
-					  Oid visimapidxid)
+					  Oid visimapidxid,
+					  int16 version)
 {
 	Relation	pg_appendonly_rel;
 	HeapTuple	pg_appendonly_tuple = NULL;
@@ -74,6 +75,7 @@ InsertAppendOnlyEntry(Oid relid,
 	values[Anum_pg_appendonly_blkdiridxid - 1] = ObjectIdGetDatum(blkdiridxid);
 	values[Anum_pg_appendonly_visimaprelid - 1] = ObjectIdGetDatum(visimaprelid);
 	values[Anum_pg_appendonly_visimapidxid - 1] = ObjectIdGetDatum(visimapidxid);
+	values[Anum_pg_appendonly_version - 1] = Int16GetDatum(version);
 
 	/*
 	 * form the tuple and insert it

greenplum-db/gpdb-archive

src/backend/catalog/pg_appendonly.c
@@ -49,7 +49,8 @@ void
 InsertAppendOnlyEntry(Oid relid,
 					  Oid segrelid,
 					  Oid blkdirrelid,
-					  Oid visimaprelid)
+					  Oid visimaprelid,
+					  int16 version)
 {
 	Relation	pg_appendonly_rel;
 	HeapTuple	pg_appendonly_tuple = NULL;
@@ -70,6 +71,7 @@ InsertAppendOnlyEntry(Oid relid,
 	values[Anum_pg_appendonly_segrelid - 1] = ObjectIdGetDatum(segrelid);
 	values[Anum_pg_appendonly_blkdirrelid - 1] = ObjectIdGetDatum(blkdirrelid);
 	values[Anum_pg_appendonly_visimaprelid - 1] = ObjectIdGetDatum(visimaprelid);
+	values[Anum_pg_appendonly_version - 1] = Int16GetDatum(version);
 
 	/*
 	 * form the tuple and insert it