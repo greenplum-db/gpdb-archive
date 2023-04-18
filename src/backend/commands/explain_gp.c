df3708ab7bd9c9c6c222cb0d785257370d14447b
QingMa <maqi@vmware.com>
2023-04-18 15:08:00 +0800

    Print all JIT instrumentation in EXPLAIN ANALYZE VERBOSE mode (#15353)
    For example:
    ```
    postgres=# explain analyze verbose select * from test limit 10;
                                                               QUERY PLAN
    ---------------------------------------------------------------------------------------------------------------------------------
     Limit  (cost=35.50..35.67 rows=10 width=4) (actual time=16.941..16.944 rows=0 loops=1)
       Output: c1
       ->  Gather Motion 3:1  (slice1; segments: 3)  (cost=35.50..36.01 rows=30 width=4) (actual time=15.436..15.438 rows=0 loops=1)
             Output: c1
             ->  Limit  (cost=35.50..35.61 rows=10 width=4) (actual time=0.000..1.317 rows=0 loops=1)
                   Output: c1
                   ->  Seq Scan on public.test  (cost=0.00..355.00 rows=32100 width=4) (actual time=0.000..0.029 rows=0 loops=1)
                         Output: c1
     Optimizer: Postgres query optimizer
     Settings: jit = 'on', jit_above_cost = '0'
     Planning Time: 2.616 ms
       (slice0)    Executor memory: 37K bytes.
       (slice1)    Executor memory: 38K bytes avg x 3 workers, 38K bytes max (seg0).
     Memory used:  128000kB
     JIT:
       Options: Inlining false, Optimization false, Expressions true, Deforming true.
       (slice0): Functions: 2.00. Timing: 1.535 ms total.
        seg-1: Function 2, Generation 0.117 ms, Inlining 0.000 ms, Optimization 0.112 ms, Emission 1.306 ms, Total 1.535 ms.
       (slice1): Functions: 1.00 avg x 3 workers, 1.00 max (seg0). Timing: 1.268 ms avg x 3 workers, 1.312 ms max (seg2).
        seg0: Function 1, Generation 0.078 ms, Inlining 0.000 ms, Optimization 0.089 ms, Emission 1.081 ms, Total 1.248 ms.
        seg1: Function 1, Generation 0.079 ms, Inlining 0.000 ms, Optimization 0.085 ms, Emission 1.081 ms, Total 1.245 ms.
        seg2: Function 1, Generation 0.079 ms, Inlining 0.000 ms, Optimization 0.089 ms, Emission 1.143 ms, Total 1.312 ms.
     Execution Time: 29.889 ms
    (23 rows)
    
    ```

--- src/backend/commands/explain_gp.c
+++ src/backend/commands/explain_gp.c
@@ -2268,6 +2268,7 @@ cdbexplain_printJITSummary(ExplainState *es, QueryDesc *queryDesc)
 	CdbExplain_SliceWorker *ssw;
 	JitInstrumentation 		*ji;
 	instr_time		 total_time;
+	StringInfo		   allstats = makeStringInfo();
 
 	/* don't print information if no JITing happened */
 	int jit_flags = queryDesc->estate->es_jit_flags;
@@ -2327,17 +2328,16 @@ cdbexplain_printJITSummary(ExplainState *es, QueryDesc *queryDesc)
 			INSTR_TIME_ADD(total_time, ji->optimization_counter);
 			INSTR_TIME_ADD(total_time, ji->emission_counter);
 
-			elog(DEBUG5, "JIT Instrumentation(slice%d seg%d): functions %zu, generation_counter %.3f ms,"
-						 "inlining_counter %.3f ms, optimization_counter %.3f ms, emission_counter %.3f ms,"
-						 "total_time %.3f ms.",
-						 slice_index,
-						 ss->segindex0 + j,
-						 ji->created_functions,
-						 1000.0 * INSTR_TIME_GET_DOUBLE(ji->generation_counter),
-						 1000.0 * INSTR_TIME_GET_DOUBLE(ji->inlining_counter),
-						 1000.0 * INSTR_TIME_GET_DOUBLE(ji->optimization_counter),
-						 1000.0 * INSTR_TIME_GET_DOUBLE(ji->emission_counter),
-						 1000.0 * INSTR_TIME_GET_DOUBLE(total_time));
+			appendStringInfoSpaces(allstats, es->indent * 2 + 1);
+			appendStringInfo(allstats,
+							 "%s%d: %s %zu, %s %.3f ms, %s %.3f ms, %s %.3f ms, %s %.3f ms, %s %.3f ms.\n",
+							 "seg", ss->segindex0 +j,
+							 "Functions", ji->created_functions,
+							 "Generation", 1000.0 * INSTR_TIME_GET_DOUBLE(ji->generation_counter),
+							 "Inlining", 1000.0 * INSTR_TIME_GET_DOUBLE(ji->inlining_counter),
+							 "Optimization", 1000.0 * INSTR_TIME_GET_DOUBLE(ji->optimization_counter),
+							 "Emission", 1000.0 * INSTR_TIME_GET_DOUBLE(ji->emission_counter),
+							 "Total", 1000.0 * INSTR_TIME_GET_DOUBLE(total_time));
 
 			avg_time += INSTR_TIME_GET_DOUBLE(total_time);
 			if (INSTR_TIME_GET_DOUBLE(total_time) > max_time)
@@ -2347,7 +2347,6 @@ cdbexplain_printJITSummary(ExplainState *es, QueryDesc *queryDesc)
 			}
 			nworker++;
 		}
-
 		// print nothing if jit is not performed on all workers in current slice
 		if (nworker == 0)
 			continue;
@@ -2373,6 +2372,10 @@ cdbexplain_printJITSummary(ExplainState *es, QueryDesc *queryDesc)
 				else
 					appendStringInfo(es->str, "%.3f ms avg x %d workers, %.3f ms max (seg%d).\n",
 									 1000.0 * avg_time, nworker, 1000.0 * max_time, ss->segindex0 + idx2);
+			}
+			if (es->verbose)
+			{
+				appendStringInfo(es->str, "%s", allstats->data);
 			}
 		}
 		else
@@ -2404,11 +2407,17 @@ cdbexplain_printJITSummary(ExplainState *es, QueryDesc *queryDesc)
 					ExplainPropertyInteger("segid", NULL, ss->segindex0 + idx2, es);
 					ExplainCloseGroup("Timing", "Timing", true, es);
 				}
+				if (es->verbose)
+					ExplainPropertyStringInfo("AllStats", es, "%s", allstats->data);
 			}
 			ExplainCloseGroup("slice", "slice", true, es);
 		}
+		resetStringInfo(allstats);
 	}
 
 	es->indent -= 1;
 	ExplainCloseGroup("JIT", "JIT", true, es);
+
+	pfree(allstats->data);
+	pfree(allstats);
 }
