24495065813f382971b40c75a5643fc79676f593
Ashwin Agrawal <aashwin@vmware.com>
2023-04-17 19:09:01 -0700

    Remove unused memtuple_binding code for AOCO
    AOCO tables don't use memtuples so no need to create or destroy
    memtuple.

--- src/backend/access/aocs/aocs_compaction.c
+++ src/backend/access/aocs/aocs_compaction.c
@@ -225,7 +225,6 @@ AOCSSegmentFileFullCompaction(Relation aorel,
 	TupleTableSlot *slot;
 	int			compact_segno;
 	ResultRelInfo *resultRelInfo;
-	MemTupleBinding *mt_bind;
 	EState	   *estate;
 	int64		moved_tupleCount = 0;
 	int64		tuplePerPage = INT_MAX;
@@ -261,8 +260,6 @@ AOCSSegmentFileFullCompaction(Relation aorel,
 	tupDesc = RelationGetDescr(aorel);
 	slot = MakeSingleTupleTableSlot(tupDesc, &TTSOpsVirtual);
 	slot->tts_tableOid = RelationGetRelid(aorel);
-
-	mt_bind = create_memtuple_binding(tupDesc);
 
 	/*
 	 * We need a ResultRelInfo and an EState so we can use the regular
@@ -356,7 +353,6 @@ AOCSSegmentFileFullCompaction(Relation aorel,
 	FreeExecutorState(estate);
 
 	ExecDropSingleTupleTableSlot(slot);
-	destroy_memtuple_binding(mt_bind);
 
 	aocs_endscan(scanDesc);
 
