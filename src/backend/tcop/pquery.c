3443781b0f5824a64454e5f76ec7c69aeb98880c
wenru yan <ywenru@vmware.com>
2023-04-28 08:40:58 +0000

    Unassign pure-catalog query from resource group
    When gp_resource_group_bypass_catalog_query is on, unassign the pure
    catalog query from resource group, and bypass the queries.
    
    Traverse the flattened rangetable entries to decide whether the query
    is pure-catalog query.
    
    In addition, it's hard to see whether the query contains UDF, traverse
    the whole plan is expensive, so it will be ignored even if there can
    have non-catalog query in the sql commands inside the UDF. But for the
    special case like 'select UDF;', the query won't be bypassed. We have
    no need to consider the sql commands inside the UDF, they will also be
    bypassed or use the same resgroup as the outer query.

--- src/backend/tcop/pquery.c
+++ src/backend/tcop/pquery.c
@@ -206,6 +206,10 @@ ProcessQuery(Portal portal,
 	if (query_info_collect_hook)
 		(*query_info_collect_hook)(METRICS_QUERY_SUBMIT, queryDesc);
 
+	if (ShouldUnassignResGroup())
+	{
+		ShouldBypassQuery(queryDesc->plannedstmt, false);
+	}
 	queryDesc->plannedstmt->query_mem = ResourceManagerGetQueryMemoryLimit(queryDesc->plannedstmt);
 
 	if (Gp_role == GP_ROLE_DISPATCH)
@@ -682,6 +686,11 @@ PortalStart(Portal portal, ParamListInfo params,
 					queryDesc->ddesc->parallelCursorName = queryDesc->portal_name;
 				}
 
+				if (ShouldUnassignResGroup())
+				{
+					bool inFunction = already_under_executor_run() || utility_nested();
+					ShouldBypassQuery(queryDesc->plannedstmt, inFunction);
+				}
 				queryDesc->plannedstmt->query_mem = ResourceManagerGetQueryMemoryLimit(queryDesc->plannedstmt);
 
 				if (Gp_role == GP_ROLE_DISPATCH)
