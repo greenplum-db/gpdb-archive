greenplum-db/gpdb-archive

commit 0c942b968db1badec64f5385e58fa971be46a461

Refactor AO macro RelationIsAppendOptimized (#15546)
This PR aims to refactor macro RelationIsAppendOptimized  which includes partition check in GP7.
Compared to GP6, now we didn't create auxiliary tables for the partition table because it doesn't
store any data in real, and that's the reason caused some bugs and it's also the motivation for refactoring it.
Removed partition check in `RelationIsAppendOptimized `, then consistent with GP6 in behavior,
add a new macro `RelationStorageIsAO` which stands for table store data in real and has auxiliary tables
except root partition table.
- For callsite of RelationStorageIsAO
if it's according to aux tables or storage, like most code related to storage and executor
- For callsite of RelationIsAppendOptimized
mainly concentrate on root partition table, like in parser and planner.

src/backend/commands/cluster.c
@@ -670,6 +670,8 @@ rebuild_relation(Relation OldHeap, Oid indexOid, bool verbose)
 	TransactionId frozenXid;
 	MultiXactId cutoffMulti;
 
+    bool		is_ao = RelationStorageIsAO(OldHeap);
+
 	/* Mark the correct index as clustered */
 	if (OidIsValid(indexOid))
 		mark_index_clustered(OldHeap, indexOid, true);

src/backend/access/appendonly/aomd.c
@@ -622,7 +622,7 @@ ao_rel_get_physical_size(Relation aorel)
 	Oid			segrelid;
 	uint64 		total_physical_size = 0;
 
-	Assert(RelationIsAppendOptimized(aorel));
+	Assert(RelationStorageIsAO(aorel));
 
 	GetAppendOnlyEntryAuxOids(aorel,
 							  &segrelid, NULL, NULL);

src/backend/access/appendonly/aosegfiles.c
@@ -1383,7 +1383,7 @@ get_ao_distribution(PG_FUNCTION_ARGS)
 		/*
 		 * verify this is an AO relation
 		 */
-		if (!RelationIsAppendOptimized(parentrel))
+		if (!RelationStorageIsAO(parentrel))
 			ereport(ERROR,
 					(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 					 errmsg("'%s' is not an append-only relation",

src/backend/access/appendonly/appendonly_blkdir_udf.c
@@ -92,7 +92,7 @@ gp_aoblkdir(PG_FUNCTION_ARGS)
 		/* initialize Context for SRF */
 		context = (Context *) palloc0(sizeof(Context));
 		context->aorel = table_open(aoRelOid, AccessShareLock);
-		if (!RelationIsAppendOptimized(context->aorel))
+		if (!RelationStorageIsAO(context->aorel))
 			ereport(ERROR,
 					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
 						errmsg("function not supported on non append-optimized relation")));

src/backend/access/appendonly/appendonly_compaction.c
@@ -133,7 +133,7 @@ AppendOnlyCompaction_ShouldCompact(Relation aoRelation,
 	double		hideRatio;
 	Oid		visimaprelid;
 
-	Assert(RelationIsAppendOptimized(aoRelation));
+	Assert(RelationStorageIsAO(aoRelation));
 	GetAppendOnlyEntryAuxOids(aoRelation,
 					NULL, NULL,
 					&visimaprelid);
@@ -542,7 +542,7 @@ AppendOptimizedCollectDeadSegments(Relation aorel)
 	Oid			segrelid;
 	Bitmapset	*dead_segs = NULL;
 
-	Assert(RelationIsAppendOptimized(aorel));
+	Assert(RelationStorageIsAO(aorel));
 
 	GetAppendOnlyEntryAuxOids(aorel,
 							  &segrelid, NULL, NULL);
@@ -694,7 +694,7 @@ AppendOptimizedTruncateToEOF(Relation aorel, AOVacuumRelStats *vacrelstats)
 	Snapshot	appendOnlyMetaDataSnapshot = RegisterSnapshot(GetCatalogSnapshot(InvalidOid));
 	Oid			segrelid;
 
-	Assert(RelationIsAppendOptimized(aorel));
+	Assert(RelationStorageIsAO(aorel));
 
 	relname = RelationGetRelationName(aorel);
 

src/backend/access/appendonly/appendonly_visimap_udf.c
@@ -81,7 +81,7 @@ gp_aovisimap(PG_FUNCTION_ARGS)
 		context = (Context *) palloc0(sizeof(Context));
 
 		context->aorel = table_open(aoRelOid, AccessShareLock);
-		if (!RelationIsAppendOptimized(context->aorel))
+		if (!RelationStorageIsAO(context->aorel))
 			ereport(ERROR,
 					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
 					 errmsg("function not supported on relation")));
@@ -195,7 +195,7 @@ gp_aovisimap_hidden_info(PG_FUNCTION_ARGS)
 		context = (Context *) palloc0(sizeof(Context));
 
 		context->parentRelation = table_open(aoRelOid, AccessShareLock);
-		if (!RelationIsAppendOptimized(context->parentRelation))
+		if (!RelationStorageIsAO(context->parentRelation))
 			ereport(ERROR,
 					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
 					 errmsg("function not supported on relation")));
@@ -372,7 +372,7 @@ gp_aovisimap_entry(PG_FUNCTION_ARGS)
 		context = (Context *) palloc0(sizeof(Context));
 
 		context->parentRelation = table_open(aoRelOid, AccessShareLock);
-		if (!RelationIsAppendOptimized(context->parentRelation))
+		if (!RelationStorageIsAO(context->parentRelation))
 			ereport(ERROR,
 					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
 					 errmsg("function not supported on relation")));

src/backend/access/brin/brin.c
@@ -188,7 +188,7 @@ brininsert(Relation idxRel, Datum *values, bool *nulls,
 	 */
 	origHeapBlk = ItemPointerGetBlockNumber(heaptid);
 	heapBlk = brin_range_start_blk(origHeapBlk,
-								   RelationIsAppendOptimized(heapRel),
+								   RelationStorageIsAO(heapRel),
 								   pagesPerRange);
 
 	/*
@@ -204,7 +204,7 @@ brininsert(Relation idxRel, Datum *values, bool *nulls,
 	 * So, we can safely position the revmap iterator at the end of the chain
 	 * (instead of traversing the chain unnecessarily from the front).
 	 */
-	if (RelationIsAppendOptimized(heapRel))
+	if (RelationStorageIsAO(heapRel))
 		brinRevmapAOPositionAtEnd(revmap, AOSegmentGet_blockSequenceNum(heapBlk));
 
 	for (;;)
@@ -496,7 +496,7 @@ bringetbitmap(IndexScanDesc scan, Node **bmNodeP)
 	BlockNumber endblknum = sequences[i].startblknum + sequences[i].nblocks;
 	int			currseq = AOSegmentGet_blockSequenceNum(startblknum);
 
-	if (RelationIsAppendOptimized(heapRel))
+	if (RelationStorageIsAO(heapRel))
 		brinRevmapAOPositionAtStart(opaque->bo_rmAccess, currseq);
 
 	for (heapBlk = startblknum; heapBlk < endblknum; heapBlk += opaque->bo_pagesPerRange)
@@ -792,7 +792,7 @@ brinbuild(Relation heap, Relation index, IndexInfo *indexInfo)
 	BlockNumber pagesPerRange;
 	bool		isAO;
 
-	isAO = RelationIsAppendOptimized(heap);
+	isAO = RelationStorageIsAO(heap);
 	/*
 	 * We expect to be called exactly once for any index relation.
 	 */
@@ -810,7 +810,7 @@ brinbuild(Relation heap, Relation index, IndexInfo *indexInfo)
 	LockBuffer(meta, BUFFER_LOCK_EXCLUSIVE);
 
 	brin_metapage_init(BufferGetPage(meta), BrinGetPagesPerRange(index),
-					   BRIN_CURRENT_VERSION, RelationIsAppendOptimized(heap));
+					   BRIN_CURRENT_VERSION, RelationStorageIsAO(heap));
 	MarkBufferDirty(meta);
 
 	if (RelationNeedsWAL(index))
@@ -1052,7 +1052,7 @@ brin_summarize_range_internal(PG_FUNCTION_ARGS)
 		SetUserIdAndSecContext(heapRel->rd_rel->relowner,
 							   save_sec_context | SECURITY_RESTRICTED_OPERATION);
 		save_nestlevel = NewGUCNestLevel();
-		if (RelationIsAppendOptimized(heapRel) && heapBlk64 != BRIN_ALL_BLOCKRANGES)
+		if (RelationStorageIsAO(heapRel) && heapBlk64 != BRIN_ALL_BLOCKRANGES)
 		{
 			ereport(ERROR,
 					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
@@ -1431,7 +1431,7 @@ summarize_range(IndexInfo *indexInfo, BrinBuildState *state, Relation heapRel,
 		 * Fortunately, this should occur infrequently.
 		 */
 
-		if (endblknum != heapNumBlks && RelationIsAppendOptimized(heapRel))
+		if (endblknum != heapNumBlks && RelationStorageIsAO(heapRel))
 		{
 			/*
 			 * GPDB: We bail and don't summarize the final partial range if we
@@ -1596,7 +1596,7 @@ brinsummarize(Relation index, Relation heapRel, BlockNumber pageRange,
 		Assert(numSequences == 1);
 
 		startBlk = brin_range_start_blk(pageRange,
-										RelationIsAppendOptimized(heapRel),
+										RelationStorageIsAO(heapRel),
 										pagesPerRange);
 		endBlk = Min(sequences[i].nblocks, startBlk + pagesPerRange);
 		if (startBlk > endBlk)
@@ -1608,7 +1608,7 @@ brinsummarize(Relation index, Relation heapRel, BlockNumber pageRange,
 		}
 	}
 
-	if (RelationIsAppendOptimized(heapRel))
+	if (RelationStorageIsAO(heapRel))
 		brinRevmapAOPositionAtStart(revmap,
 									AOSegmentGet_blockSequenceNum(startBlk));
 
@@ -1645,7 +1645,7 @@ brinsummarize(Relation index, Relation heapRel, BlockNumber pageRange,
 				Assert(!indexInfo);
 				state = initialize_brin_buildstate(index, revmap,
 												   pagesPerRange,
-												   RelationIsAppendOptimized(heapRel));
+												   RelationStorageIsAO(heapRel));
 				indexInfo = BuildIndexInfo(index);
 			}
 			summarize_range(indexInfo, state, heapRel, startBlk, endBlk);

src/backend/access/table/tableam.c
@@ -209,7 +209,7 @@ table_index_fetch_tuple_check(Relation rel,
 	 * overhead is significant for AO/CO relations. For details, please refer to
 	 * table_index_unique_check().
 	 */
-	if (RelationIsAppendOptimized(rel))
+	if (RelationStorageIsAO(rel))
 		return table_index_unique_check(rel, tid, snapshot, all_dead);
 
 	slot = table_slot_create(rel, NULL);

commit 5fc2f42f3753692b312333bb768d88ea7314d4e2

fix compile warning after refactoring macro RelationIsAppendOptimized (#15661)
Fix compile warning after refactoring macro RelationIsAppendOptimized caused by 0c942b9.

src/backend/commands/cluster.c
@@ -670,8 +670,6 @@ rebuild_relation(Relation OldHeap, Oid indexOid, bool verbose)
 	TransactionId frozenXid;
 	MultiXactId cutoffMulti;
 
-    bool		is_ao = RelationStorageIsAO(OldHeap);
-
 	/* Mark the correct index as clustered */
 	if (OidIsValid(indexOid))
 		mark_index_clustered(OldHeap, indexOid, true);