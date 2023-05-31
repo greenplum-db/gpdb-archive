4b85ea8bd33a99babbaba4c3d969cd37fb6e5621
zhjwpku <zhjwpku@gmail.com>
2023-05-31 10:24:20 +0800

    [typo] polish parameter name of _get_remove_cmd (#15473)
    The paramter `cmd` of _get_remove_cmd failed to express its meaning,
    seems better renaming it to `removeFile`.
    
    Signed-off-by: Junwang Zhao <zhjwpku@gmail.com>

--- gpMgmt/bin/gppylib/operations/buildMirrorSegments.py
+++ gpMgmt/bin/gppylib/operations/buildMirrorSegments.py
@@ -477,8 +477,8 @@ class GpMirrorListToBuild:
                                                        remoteHost=targetHostname)
         return None
 
-    def _get_remove_cmd(self, cmd, target_host):
-        return base.Command("remove file", "rm -f {}".format(pipes.quote(cmd)), ctxt=base.REMOTE, remoteHost=target_host)
+    def _get_remove_cmd(self, remove_file, target_host):
+        return base.Command("remove file", "rm -f {}".format(pipes.quote(remove_file)), ctxt=base.REMOTE, remoteHost=target_host)
 
     def __runWaitAndCheckWorkerPoolForErrorsAndClear(self, cmds, suppressErrorCheck=False, progressCmds=[]):
         for cmd in cmds:
