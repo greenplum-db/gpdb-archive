5cae067e41e33e78b96db2391c2f713292106483
Yongtao Huang <99629139+hyongtao-db@users.noreply.github.com>
2023-06-08 15:42:04 +0800

    FIX BUG: COPY FORM doesn't throw out ERROR: extra data after last expected column (7X) (#15612)
    This commit can fix bug reported in [issue 15625]
    (https://github.com/greenplum-db/gpdb/issues/15625)
    
    **Solution：**
    In pg12.12, there is no function `InitCopyFromDispatchSplit()`, 
    so attr_count must be > 0. In GP7, we may get `[NULL]` 
    in QE when all fields are processed in the QD. 
    So the condition checking should change 
    from `attr_count > 0` to `attr_count >= 0`. 
    But before it, `attr_count = list_length(attnumlist);`, 
    we can ensure that `attr_count >= 0` consistently, 
    so delete this bug-related condition `attr_count > 0`.
    
    Signed-off-by: Yongtao Huang <yongtaoh@vmware.com>
    Co-authored-by: Xiaoxiao He <hxiaoxiao@vmware.com>

--- src/backend/commands/copy.c
+++ src/backend/commands/copy.c
@@ -5526,8 +5526,14 @@ NextCopyFromX(CopyState cstate, ExprContext *econtext,
 			field_strings = cstate->raw_fields;
 		}
 
-		/* check for overflowing fields */
-		if (attr_count > 0 && fldct > attr_count)
+		/* 
+		 * Check for overflowing fields.
+		 * GPDB: Change below condition compared to upstream to 
+		 * greater than or equal to 0 as in QE, 
+		 * attr_count may be equal to 0, 
+		 * when all fields are processed in the QD.
+		 */
+		if (fldct > attr_count)
 			ereport(ERROR,
 					(errcode(ERRCODE_BAD_COPY_FILE_FORMAT),
 					 errmsg("extra data after last expected column")));
