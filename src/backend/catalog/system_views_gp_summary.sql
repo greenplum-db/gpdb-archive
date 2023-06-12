b0f26b1b7062eb9af1af110a0852ee21dda0a193
Brent Doil <bdoil@vmware.com>
2023-06-12 14:17:16 -0400

    Fix gp_statio_user_tables_summary view

--- src/backend/catalog/system_views_gp_summary.sql
+++ src/backend/catalog/system_views_gp_summary.sql
@@ -292,7 +292,7 @@ CREATE VIEW gp_statio_sys_tables_summary AS
           schemaname ~ '^pg_toast';
 
 CREATE VIEW gp_statio_user_tables_summary AS
-    SELECT * FROM gp_stat_all_tables_summary
+    SELECT * FROM gp_statio_all_tables_summary
     WHERE schemaname NOT IN ('pg_catalog', 'information_schema', 'pg_aoseg') AND
           schemaname !~ '^pg_toast';
 
--- src/include/catalog/catversion.h
+++ src/include/catalog/catversion.h
@@ -56,6 +56,6 @@
  */
 
 /*							3yyymmddN */
-#define CATALOG_VERSION_NO	302305301
+#define CATALOG_VERSION_NO	302306121
 
 #endif
