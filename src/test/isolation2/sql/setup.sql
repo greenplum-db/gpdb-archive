b8d845a9b582e96d33d54c00ba8732db5bc96be0
Zhang Hao <hzhang2@vmware.com>
2023-07-11 17:42:23 +0800

    Fix the warning of pg_basebackup(). (#15784)
    When an error occurs in pg_basebackup(), its detail can not be
    printed properly. Instead, the following error will be raised:
    
    ```
    +ERROR:  TypeError: can only concatenate str (not "bytes") to str
    +CONTEXT:  Traceback (most recent call last):
    +  PL/Python function "pg_basebackup", line 26, in <module>
    +    results = str(e) + "\ncommand output: " + e.output
    +PL/Python function "pg_basebackup"
    ```
    This problem is caused by that e.output is a bytes array and 
    cannot contact str.
    
    The solution is to change e.output to e.output.decode().

--- src/test/isolation2/sql/setup.sql
+++ src/test/isolation2/sql/setup.sql
@@ -363,7 +363,7 @@ create or replace function pg_basebackup(host text, dbid int, port int, create_s
             os.environ.pop('PGAPPNAME')
         results = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True).replace(b'.', b'').decode()
     except subprocess.CalledProcessError as e:
-        results = str(e) + "\ncommand output: " + e.output
+        results = str(e) + "\ncommand output: " + (e.output.decode())
 
     return results
 $$ language plpython3u;
