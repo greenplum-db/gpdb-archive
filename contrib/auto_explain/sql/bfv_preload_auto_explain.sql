1215107a6fa0bd1743071055d983340fea950a16
Ru Yang <ruyang@pivotal.io>
2023-08-07 15:08:02 +0800

    Append to shared_preload_libraries instead of replace (#16135)
    Previously in ICW tests, some extensions include auto_explain and
    pgcrypto set GUC `shared_preload_libraries` regardless of its
    original value. This will impact other extensions who also need to
    include themselves into `shared_preload_libraries`.
    
    To fix this issue, this patch changes the `gpconfig` command to
    
    - Before running the test, append new library names to
      the end, rather than replace the whole value.
    - After running the test, instead of removing the GUC setting, only
      the newly appended libraries are removed.
    
    As a result, extensions required to be in `shared_preload_libraries`
    can work nicely together throughout the whole ICW tests.
    
    
    Co-authored-by: Xuebin Su <sxuebin@vmware.com>

--- contrib/auto_explain/sql/bfv_preload_auto_explain.sql
+++ contrib/auto_explain/sql/bfv_preload_auto_explain.sql
@@ -1,5 +1,5 @@
 -- start_ignore
-\! gpconfig -c shared_preload_libraries -v 'auto_explain';
+\! gpconfig -c shared_preload_libraries -v "$(psql -At -c "SELECT array_to_string(array_append(string_to_array(current_setting('shared_preload_libraries'), ','), 'auto_explain'), ',')" postgres)"
 \! gpconfig -c auto_explain.log_min_duration -v 0 --skipvalidation;
 \! gpconfig -c auto_explain.log_analyze -v true --skipvalidation;
 \! gpstop -raiq;
@@ -27,6 +27,6 @@ DROP TABLE t2;
 -- start_ignore
 \! gpconfig -r auto_explain.log_min_duration;
 \! gpconfig -r auto_explain.log_analyze;
-\! gpconfig -r shared_preload_libraries;
+\! gpconfig -c shared_preload_libraries -v "$(psql -At -c "SELECT array_to_string(array_remove(string_to_array(current_setting('shared_preload_libraries'), ','), 'auto_explain'), ',')" postgres)"
 \! gpstop -raiq;
 -- end_ignore
--- contrib/pgcrypto/sql/setup_fips.sql
+++ contrib/pgcrypto/sql/setup_fips.sql
@@ -4,6 +4,6 @@
 -- 'pgcrypto.fips' immediately after creating the extension on master.
 
 -- start_ignore
-\! gpconfig -c shared_preload_libraries -v 'pgcrypto.so'
+\! gpconfig -c shared_preload_libraries -v "$(psql -At -c "SELECT array_to_string(array_append(string_to_array(current_setting('shared_preload_libraries'), ','), 'pgcrypto.so'), ',')" postgres)"
 \! gpstop -air
 -- end_ignore
