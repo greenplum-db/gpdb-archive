e38d3e77d42c9da6b3b20a4600d6f25f54884920
Brent Doil <bdoil@vmware.com>
2023-08-22 21:59:52 -0400

    Update binary swap test files
    Authored-by: Brent Doil <bdoil@vmware.com>

--- src/test/binary_swap/sql/inserts.sql
+++ src/test/binary_swap/sql/inserts.sql
@@ -4,4 +4,3 @@
 -- Following tables from freeze_aux_tables:
 INSERT INTO test_table_heap_with_toast SELECT i, i*2, i*5 FROM generate_series(1, 20)i;
 INSERT INTO test_table_ao_with_toast SELECT * FROM test_table_heap_with_toast;
-INSERT INTO test_table_co_with_toast SELECT * FROM test_table_heap_with_toast;
