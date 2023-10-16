greenplum-db/gpdb-archive

commit 8915cd09333961e203baf0255ca1a0f2c4d8607e

Check peer listener failed in IC-PROXY mode (#16438)
In IC-PROXY mode, user doesn't get a notification when peer listener bind/listen failed. Because the related code is in IC-PROXY process, it only records warning logs. So, the user's query just hangs here silently. This behavior may make the user very confused.

In the commit, introduced a SHM variable to check the failure and give notification in time. An output example:
```
# using nc to occupy one ic-proxy process port
$ nc -l 2001
...
$ PGOPTIONS="-c gp_interconnect_type=proxy" psql demo
demo=# select count(*) from test;
ERROR:  SetupInterconnect: We are in IC_PROXY mode, but IC-Proxy Listener failed, please check. (ic_tcp.c:1296)  (seg1 slice1 127.0.1.1:6001 pid=189355) (ic_tcp.c:1296)
```

src/backend/cdb/motion/ic_proxy_backend.c
@@ -34,6 +34,7 @@
 #include "cdb/cdbvars.h"
 #include "cdb/ml_ipc.h"
 #include "executor/execdesc.h"
+#include "storage/shmem.h"
 
 #include "ic_proxy.h"
 #include "ic_proxy_backend.h"
@@ -515,6 +516,25 @@ ic_proxy_backend_init_context(ChunkTransportState *state)
 	uv_timer_init(&context->loop, &context->connectTimer);
 	uv_timer_start(&context->connectTimer, ic_proxy_backend_on_connect_timer, 0, CONNECT_TIMER_TIMEOUT);
 	uv_unref((uv_handle_t *)&context->connectTimer);
+}
+
+/*
+ * Check if current Segment's IC_PROXY listener failed
+ */
+bool
+ic_proxy_backend_check_listener_failed(void)
+{
+	bool found;
+	ic_proxy_peer_listener_failed = ShmemInitStruct("IC_PROXY Listener Failure Flag",
+											sizeof(*ic_proxy_peer_listener_failed),
+											&found);
+
+	Assert(ic_proxy_peer_listener_failed != NULL);
+	/* init it to 0 when the backend accesses it firstly */
+	if (!found)
+		pg_atomic_init_u32(ic_proxy_peer_listener_failed, 0);
+
+	return pg_atomic_read_u32(ic_proxy_peer_listener_failed) > 0;
 }
 
 /*

src/backend/cdb/motion/ic_proxy_backend.h
@@ -13,6 +13,7 @@
 #define IC_PROXY_BACKEND_H
 
 #include "postgres.h"
+#include "port/atomics.h"
 
 #include "cdb/cdbinterconnect.h"
 
@@ -38,6 +39,8 @@ typedef struct ICProxyBackendContext
 	ChunkTransportState   *transportState;
 } ICProxyBackendContext;
 
+extern pg_atomic_uint32 *ic_proxy_peer_listener_failed;
+
 extern void ic_proxy_backend_connect(ICProxyBackendContext *context,
 									 ChunkTransportStateEntry *pEntry,
 									 MotionConn *conn, bool isSender);
@@ -45,5 +48,6 @@ extern void ic_proxy_backend_connect(ICProxyBackendContext *context,
 extern void ic_proxy_backend_init_context(ChunkTransportState *state);
 extern void ic_proxy_backend_close_context(ChunkTransportState *state);
 extern void ic_proxy_backend_run_loop(ICProxyBackendContext *context);
+extern bool ic_proxy_backend_check_listener_failed(void);
 
 #endif   /* IC_PROXY_BACKEND_H */

src/backend/cdb/motion/ic_proxy_main.c
@@ -18,6 +18,7 @@
 #include "storage/ipc.h"
 #include "utils/guc.h"
 #include "utils/memutils.h"
+#include "storage/shmem.h"
 
 #include "ic_proxy_server.h"
 #include "ic_proxy_addr.h"
@@ -39,6 +40,8 @@ static uv_timer_t	ic_proxy_server_timer;
 static uv_tcp_t		ic_proxy_peer_listener;
 static bool			ic_proxy_peer_listening;
 static bool			ic_proxy_peer_relistening;
+/* flag (in SHM) for incidaing if peer listener bind/listen failed */
+pg_atomic_uint32 	*ic_proxy_peer_listener_failed;
 
 static uv_pipe_t	ic_proxy_client_listener;
 static bool			ic_proxy_client_listening;
@@ -154,8 +157,12 @@ ic_proxy_server_peer_listener_init(uv_loop_t *loop)
 	if (ic_proxy_addrs == NIL)
 		return;
 
+	Assert(ic_proxy_peer_listener_failed != NULL);
 	if (ic_proxy_peer_listening)
+	{
+		Assert(pg_atomic_read_u32(ic_proxy_peer_listener_failed) == 0);
 		return;
+	}
 
 	/* Get the addr from the gp_interconnect_proxy_addresses */
 	addr = ic_proxy_get_my_addr();
@@ -195,6 +202,7 @@ ic_proxy_server_peer_listener_init(uv_loop_t *loop)
 	{
 		elog(WARNING, "ic-proxy: tcp: failed to bind: %s",
 					 uv_strerror(ret));
+		pg_atomic_exchange_u32(ic_proxy_peer_listener_failed, 1);
 		return;
 	}
 
@@ -204,6 +212,7 @@ ic_proxy_server_peer_listener_init(uv_loop_t *loop)
 	{
 		elog(WARNING, "ic-proxy: tcp: failed to listen: %s",
 					 uv_strerror(ret));
+		pg_atomic_exchange_u32(ic_proxy_peer_listener_failed, 1);
 		return;
 	}
 
@@ -211,6 +220,7 @@ ic_proxy_server_peer_listener_init(uv_loop_t *loop)
 	elogif(gp_log_interconnect >= GPVARS_VERBOSITY_VERBOSE, LOG,
 		   "ic-proxy: tcp: listening on socket %d", fd);
 
+	pg_atomic_exchange_u32(ic_proxy_peer_listener_failed, 0);
 	ic_proxy_peer_listening = true;
 }
 
@@ -527,9 +537,18 @@ int
 ic_proxy_server_main(void)
 {
 	char		path[MAXPGPATH];
-
+	bool		found;
 	elogif(gp_log_interconnect >= GPVARS_VERBOSITY_TERSE,
 		   LOG, "ic-proxy: server setting up");
+
+	/* get and init failure flag */
+	ic_proxy_peer_listener_failed = ShmemInitStruct("IC_PROXY Listener Failure Flag",
+													sizeof(*ic_proxy_peer_listener_failed),
+													&found);
+	if (!found)
+		pg_atomic_init_u32(ic_proxy_peer_listener_failed, 0);
+	else
+		pg_atomic_exchange_u32(ic_proxy_peer_listener_failed, 0);
 
 	ic_proxy_pkt_cache_init(IC_PROXY_MAX_PKT_SIZE);
 

src/backend/cdb/motion/ic_tcp.c
@@ -1292,6 +1292,8 @@ SetupTCPInterconnect(EState *estate)
 	interconnect_context->doSendStopMessage = doSendStopMessageTCP;
 
 #ifdef ENABLE_IC_PROXY
+	if (ic_proxy_backend_check_listener_failed())
+		elog(ERROR, "SetupInterconnect: We are in IC_PROXY mode, but IC-Proxy Listener failed, please check.");
 	ic_proxy_backend_init_context(interconnect_context);
 #endif /* ENABLE_IC_PROXY */
 

src/test/isolation2/expected/ic_proxy_listen_failed.out
@@ -1,0 +1,93 @@
+-- Test case for the scenario which ic-proxy peer listener port has been occupied
+
+-- start_matchsubs
+-- m/ic_tcp.c:\d+/
+-- s/ic_tcp.c:\d+/ic_tcp.c:LINE/
+-- end_matchsubs
+
+1:create table PR_16438 (i int);
+CREATE TABLE
+1:insert into PR_16438 select generate_series(1,100);
+INSERT 0 100
+1q: ... <quitting>
+
+-- get one port and occupy it (start_py_httpserver.sh), then restart cluster
+-- start_ignore
+! ic_proxy_port=`psql postgres -Atc "show gp_interconnect_proxy_addresses;" | awk -F ',' '{print $1}' | awk -F ':' '{print $4}'` && gpstop -ai && ./script/start_py_httpserver.sh $ic_proxy_port;
+20230917:16:14:48:484878 gpstop:gpdb:ubuntu-[INFO]:-Starting gpstop with args: -ai
+20230917:16:14:48:484878 gpstop:gpdb:ubuntu-[INFO]:-Gathering information and validating the environment...
+20230917:16:14:48:484878 gpstop:gpdb:ubuntu-[INFO]:-Obtaining Greenplum Coordinator catalog information
+20230917:16:14:48:484878 gpstop:gpdb:ubuntu-[INFO]:-Obtaining Segment details from coordinator...
+20230917:16:14:48:484878 gpstop:gpdb:ubuntu-[INFO]:-Greenplum Version: 'postgres (Greenplum Database) 7.0.0-alpha.0+dev.19124.g5c36a44ab0 build dev'
+20230917:16:14:48:484878 gpstop:gpdb:ubuntu-[INFO]:-Commencing Coordinator instance shutdown with mode='immediate'
+20230917:16:14:48:484878 gpstop:gpdb:ubuntu-[INFO]:-Coordinator segment instance directory=/home/ubuntu/gp/data7/master/gpseg-1
+20230917:16:14:49:484878 gpstop:gpdb:ubuntu-[INFO]:-Attempting forceful termination of any leftover coordinator process
+20230917:16:14:49:484878 gpstop:gpdb:ubuntu-[INFO]:-Terminating processes for segment /home/ubuntu/gp/data7/master/gpseg-1
+20230917:16:14:52:484878 gpstop:gpdb:ubuntu-[INFO]:-No standby coordinator host configured
+20230917:16:14:52:484878 gpstop:gpdb:ubuntu-[INFO]:-Targeting dbid [2, 5, 3, 6, 4, 7] for shutdown
+20230917:16:14:52:484878 gpstop:gpdb:ubuntu-[INFO]:-Commencing parallel primary segment instance shutdown, please wait...
+20230917:16:14:52:484878 gpstop:gpdb:ubuntu-[INFO]:-0.00% of jobs completed
+20230917:16:14:56:484878 gpstop:gpdb:ubuntu-[INFO]:-100.00% of jobs completed
+20230917:16:14:56:484878 gpstop:gpdb:ubuntu-[INFO]:-Commencing parallel mirror segment instance shutdown, please wait...
+20230917:16:14:56:484878 gpstop:gpdb:ubuntu-[INFO]:-0.00% of jobs completed
+20230917:16:14:58:484878 gpstop:gpdb:ubuntu-[INFO]:-100.00% of jobs completed
+20230917:16:14:58:484878 gpstop:gpdb:ubuntu-[INFO]:-----------------------------------------------------
+20230917:16:14:58:484878 gpstop:gpdb:ubuntu-[INFO]:-   Segments stopped successfully      = 6
+20230917:16:14:58:484878 gpstop:gpdb:ubuntu-[INFO]:-   Segments with errors during stop   = 0
+20230917:16:14:58:484878 gpstop:gpdb:ubuntu-[INFO]:-----------------------------------------------------
+20230917:16:14:58:484878 gpstop:gpdb:ubuntu-[INFO]:-Successfully shutdown 6 of 6 segment instances 
+20230917:16:14:58:484878 gpstop:gpdb:ubuntu-[INFO]:-Database successfully shutdown with no errors reported
+started a http server
+
+! sleep 2 && gpstart -a;
+20230917:16:15:00:488914 gpstart:gpdb:ubuntu-[INFO]:-Starting gpstart with args: -a
+20230917:16:15:00:488914 gpstart:gpdb:ubuntu-[INFO]:-Gathering information and validating the environment...
+20230917:16:15:00:488914 gpstart:gpdb:ubuntu-[INFO]:-Greenplum Binary Version: 'postgres (Greenplum Database) 7.0.0-alpha.0+dev.19124.g5c36a44ab0 build dev'
+20230917:16:15:00:488914 gpstart:gpdb:ubuntu-[INFO]:-Greenplum Catalog Version: '302307241'
+20230917:16:15:00:488914 gpstart:gpdb:ubuntu-[INFO]:-Starting Coordinator instance in admin mode
+20230917:16:15:00:488914 gpstart:gpdb:ubuntu-[INFO]:-CoordinatorStart pg_ctl cmd is env GPSESSID=0000000000 GPERA=None $GPHOME/bin/pg_ctl -D /home/ubuntu/gp/data7/master/gpseg-1 -l /home/ubuntu/gp/data7/master/gpseg-1/log/startup.log -w -t 600 -o " -c gp_role=utility " start
+20230917:16:15:01:488914 gpstart:gpdb:ubuntu-[INFO]:-Obtaining Greenplum Coordinator catalog information
+20230917:16:15:01:488914 gpstart:gpdb:ubuntu-[INFO]:-Obtaining Segment details from coordinator...
+20230917:16:15:01:488914 gpstart:gpdb:ubuntu-[INFO]:-Setting new coordinator era
+20230917:16:15:01:488914 gpstart:gpdb:ubuntu-[INFO]:-Coordinator Started...
+20230917:16:15:01:488914 gpstart:gpdb:ubuntu-[INFO]:-Shutting down coordinator
+20230917:16:15:04:488914 gpstart:gpdb:ubuntu-[INFO]:-Commencing parallel primary and mirror segment instance startup, please wait...
+.
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-Process results...
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-----------------------------------------------------
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-   Successful segment starts                                            = 6
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-   Failed segment starts                                                = 0
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-   Skipped segment starts (segments are marked down in configuration)   = 0
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-----------------------------------------------------
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-Successfully started 6 of 6 segment instances 
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-----------------------------------------------------
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-Starting Coordinator instance gpdb directory /home/ubuntu/gp/data7/master/gpseg-1 
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-CoordinatorStart pg_ctl cmd is env GPSESSID=0000000000 GPERA=c25b3b36b0fe1320_230917161501 $GPHOME/bin/pg_ctl -D /home/ubuntu/gp/data7/master/gpseg-1 -l /home/ubuntu/gp/data7/master/gpseg-1/log/startup.log -w -t 600 -o " -c gp_role=dispatch " start
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-Command pg_ctl reports Coordinator gpdb instance active
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-Connecting to db template1 on host localhost
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-No standby coordinator configured.  skipping...
+20230917:16:15:05:488914 gpstart:gpdb:ubuntu-[INFO]:-Database successfully started
+
+-- end_ignore
+
+-- execute a query (should failed)
+2&:select count(*) from PR_16438;  <waiting ...>
+FAILED:  Forked command is not blocking; got output: ERROR:  SetupInterconnect: We are in IC_PROXY mode, but IC-Proxy Listener failed, please check. (ic_tcp.c:1296)
+2<:  <... completed>
+FAILED:  Execution failed
+
+-- kill the script to release port and execute query again (should successfully)
+-- start_ignore
+! ps aux | grep http.server | grep -v grep | awk '{print $2}' | xargs kill;
+
+! sleep 2;
+
+-- end_ignore
+3:select count(*) from PR_16438;
+ count 
+-------
+ 100   
+(1 row)
+3:drop table PR_16438;
+DROP TABLE

src/test/isolation2/isolation2_ic_proxy_schedule
@@ -7,3 +7,6 @@ test: tcp_ic_teardown
 
 # test TCP proxy peer shutdown
 test: ic_proxy_peer_shutdown
+
+# test ic-proxy listen failed
+test: ic_proxy_listen_failed

src/test/isolation2/script/start_py_httpserver.sh
@@ -1,0 +1,21 @@
+#!/bin/bash
+# start a python http server (port is $1) in background
+
+which python3 > /dev/null
+if [ $? -eq 0 ] 
+then
+    python3 -m http.server $1 >/dev/null 2>&1 &
+    echo "started a http server"
+    exit 0
+fi
+
+which python2 > /dev/null
+if [ $? -eq 0 ] 
+then
+    python2 -m SimpleHTTPServer $1 >/dev/null 2>&1 &
+    echo "started a http server"
+    exit 0
+fi
+
+echo "no python found"
+exit 1

src/test/isolation2/sql/ic_proxy_listen_failed.sql
@@ -1,0 +1,28 @@
+-- Test case for the scenario which ic-proxy peer listener port has been occupied
+
+-- start_matchsubs
+-- m/ic_tcp.c:\d+/
+-- s/ic_tcp.c:\d+/ic_tcp.c:LINE/
+-- end_matchsubs
+
+1:create table PR_16438 (i int);
+1:insert into PR_16438 select generate_series(1,100);
+1q:
+
+-- get one port and occupy it (start_py_httpserver.sh), then restart cluster
+-- start_ignore
+! ic_proxy_port=`psql postgres -Atc "show gp_interconnect_proxy_addresses;" | awk -F ',' '{print $1}' | awk -F ':' '{print $4}'` && gpstop -ai && ./script/start_py_httpserver.sh $ic_proxy_port;
+! sleep 2 && gpstart -a;
+-- end_ignore
+
+-- execute a query (should failed)
+2&:select count(*) from PR_16438;
+2<:
+
+-- kill the script to release port and execute query again (should successfully)
+-- start_ignore
+! ps aux | grep http.server | grep -v grep | awk '{print $2}' | xargs kill;
+! sleep 2;
+-- end_ignore
+3:select count(*) from PR_16438;
+3:drop table PR_16438;

commit 13a4c031202a56bfbc4dacff6e8e7724a787705c

Init ICProxy's SHM variable in CreateSharedMemoryAndSemaphores() (#16586)
My previous commit 8915cd0 caused coredump in some pipeline jobs.
Example stack:
```
Core was generated by `postgres:  7000, ic proxy process
Program terminated with signal SIGSEGV, Segmentation fault.
#0  0x0000000000b46ec3 in pg_atomic_read_u32_impl (ptr=0x7f05a8c51104) at ../../../../src/include/port/atomics/generic.h:48
(gdb) bt
#0  0x0000000000b46ec3 in pg_atomic_read_u32_impl (ptr=0x7f05a8c51104) at ../../../../src/include/port/atomics/generic.h:48
#1  pg_atomic_read_u32 (ptr=0x7f05a8c51104) at ../../../../src/include/port/atomics.h:247
#2  LWLockAttemptLock (mode=LW_EXCLUSIVE, lock=0x7f05a8c51100) at lwlock.c:751
#3  LWLockAcquire (lock=0x7f05a8c51100, mode=mode@entry=LW_EXCLUSIVE) at lwlock.c:1188
#4  0x0000000000b32fff in ShmemInitStruct (name=name@entry=0x130e160 "", size=size@entry=4, foundPtr=foundPtr@entry=0x7ffcf94513bf) at shmem.c:412
#5  0x0000000000d6d18e in ic_proxy_server_main () at ic_proxy_main.c:545
#6  0x0000000000d6c219 in ICProxyMain (main_arg=<optimized out>) at ic_proxy_bgworker.c:36
#7  0x0000000000aa9caa in StartBackgroundWorker () at bgworker.c:955
#8  0x0000000000ab9407 in do_start_bgworker (rw=<optimized out>) at postmaster.c:6450
#9  maybe_start_bgworkers () at postmaster.c:6706
#10 0x0000000000abbc59 in ServerLoop () at postmaster.c:2095
#11 0x0000000000abd777 in PostmasterMain (argc=argc@entry=5, argv=argv@entry=0x36e3650) at postmaster.c:1633
#12 0x00000000006e4764 in main (argc=5, argv=0x36e3650) at main.c:240
(gdb) p *ptr
Cannot access memory at address 0x7f05a8c51104
```

The root cause is I forgot to init SHM structure at CreateSharedMemoryAndSemaphores().
Fix it in this commit.

src/backend/cdb/motion/ic_proxy_backend.c
@@ -519,25 +519,6 @@ ic_proxy_backend_init_context(ChunkTransportState *state)
 }
 
 /*
- * Check if current Segment's IC_PROXY listener failed
- */
-bool
-ic_proxy_backend_check_listener_failed(void)
-{
-	bool found;
-	ic_proxy_peer_listener_failed = ShmemInitStruct("IC_PROXY Listener Failure Flag",
-											sizeof(*ic_proxy_peer_listener_failed),
-											&found);
-
-	Assert(ic_proxy_peer_listener_failed != NULL);
-	/* init it to 0 when the backend accesses it firstly */
-	if (!found)
-		pg_atomic_init_u32(ic_proxy_peer_listener_failed, 0);
-
-	return pg_atomic_read_u32(ic_proxy_peer_listener_failed) > 0;
-}
-
-/*
  * Close the icproxy backend context
  */
 void

src/backend/cdb/motion/ic_proxy_backend.h
@@ -13,7 +13,6 @@
 #define IC_PROXY_BACKEND_H
 
 #include "postgres.h"
-#include "port/atomics.h"
 
 #include "cdb/cdbinterconnect.h"
 
@@ -48,6 +47,5 @@ extern void ic_proxy_backend_connect(ICProxyBackendContext *context,
 extern void ic_proxy_backend_init_context(ChunkTransportState *state);
 extern void ic_proxy_backend_close_context(ChunkTransportState *state);
 extern void ic_proxy_backend_run_loop(ICProxyBackendContext *context);
-extern bool ic_proxy_backend_check_listener_failed(void);
 
 #endif   /* IC_PROXY_BACKEND_H */

src/backend/cdb/motion/ic_proxy_bgworker.c
@@ -16,6 +16,7 @@
 #include "postgres.h"
 
 #include "storage/ipc.h"
+#include "storage/shmem.h"
 
 #include "cdb/ic_proxy_bgworker.h"
 #include "ic_proxy_server.h"
@@ -34,4 +35,29 @@ ICProxyMain(Datum main_arg)
 {
 	/* main loop */
 	proc_exit(ic_proxy_server_main());
+}
+
+/*
+ * the size of ICProxy SHM structure
+ */
+Size
+ICProxyShmemSize(void)
+{
+	Size		size = 0;
+	size = add_size(size, sizeof(*ic_proxy_peer_listener_failed));
+	return size;
+}
+
+/*
+ * initialize ICProxy's SHM structure: only one flag variable
+ */
+void
+ICProxyShmemInit(void)
+{
+	bool		found;
+	ic_proxy_peer_listener_failed = ShmemInitStruct("IC_PROXY Listener Failure Flag",
+													sizeof(*ic_proxy_peer_listener_failed),
+													&found);
+	if (!found)
+		pg_atomic_init_u32(ic_proxy_peer_listener_failed, 0);
 }

src/backend/cdb/motion/ic_proxy_main.c
@@ -537,19 +537,10 @@ int
 ic_proxy_server_main(void)
 {
 	char		path[MAXPGPATH];
-	bool		found;
 	elogif(gp_log_interconnect >= GPVARS_VERBOSITY_TERSE,
 		   LOG, "ic-proxy: server setting up");
 
-	/* get and init failure flag */
-	ic_proxy_peer_listener_failed = ShmemInitStruct("IC_PROXY Listener Failure Flag",
-													sizeof(*ic_proxy_peer_listener_failed),
-													&found);
-	if (!found)
-		pg_atomic_init_u32(ic_proxy_peer_listener_failed, 0);
-	else
-		pg_atomic_exchange_u32(ic_proxy_peer_listener_failed, 0);
-
+	pg_atomic_exchange_u32(ic_proxy_peer_listener_failed, 0);
 	ic_proxy_pkt_cache_init(IC_PROXY_MAX_PKT_SIZE);
 
 	uv_loop_init(&ic_proxy_server_loop);

src/backend/cdb/motion/ic_tcp.c
@@ -1292,7 +1292,8 @@ SetupTCPInterconnect(EState *estate)
 	interconnect_context->doSendStopMessage = doSendStopMessageTCP;
 
 #ifdef ENABLE_IC_PROXY
-	if (ic_proxy_backend_check_listener_failed())
+	/* check if current Segment's ICProxy listener failed */
+	if (pg_atomic_read_u32(ic_proxy_peer_listener_failed) > 0)
 		elog(ERROR, "SetupInterconnect: We are in IC_PROXY mode, but IC-Proxy Listener failed, please check.");
 	ic_proxy_backend_init_context(interconnect_context);
 #endif /* ENABLE_IC_PROXY */

src/backend/postmaster/postmaster.c
@@ -428,11 +428,7 @@ static BackgroundWorker PMAuxProcList[MaxPMAuxProc] =
 
 #ifdef ENABLE_IC_PROXY
 	{"ic proxy process", "ic proxy process",
-#ifdef FAULT_INJECTOR
 	 BGWORKER_SHMEM_ACCESS,
-#else
-	 0,
-#endif
 	 BgWorkerStart_RecoveryFinished,
 	 0, /* restart immediately if ic proxy process exits with non-zero code */
 	 "postgres", "ICProxyMain", 0, {0}, 0,

src/backend/storage/ipc/ipci.c
@@ -70,6 +70,7 @@
 #include "utils/session_state.h"
 #include "cdb/cdbendpoint.h"
 #include "replication/gp_replication.h"
+#include "cdb/ic_proxy_bgworker.h"
 
 /* GUCs */
 int			shared_memory_type = DEFAULT_SHARED_MEMORY_TYPE;
@@ -200,6 +201,10 @@ CreateSharedMemoryAndSemaphores(int port)
 #ifdef FAULT_INJECTOR
 		size = add_size(size, FaultInjector_ShmemSize());
 #endif			
+
+#ifdef ENABLE_IC_PROXY
+		size = add_size(size, ICProxyShmemSize());
+#endif
 
 		/* This elog happens before we know the name of the log file we are supposed to use */
 		elog(DEBUG1, "Size not including the buffer pool %lu",
@@ -353,6 +358,10 @@ CreateSharedMemoryAndSemaphores(int port)
 
 #ifdef FAULT_INJECTOR
 	FaultInjector_ShmemInit();
+#endif
+
+#ifdef ENABLE_IC_PROXY
+	ICProxyShmemInit();
 #endif
 
 	/*

src/include/cdb/ic_proxy_bgworker.h
@@ -13,7 +13,14 @@
 #ifndef IC_PROXY_BGWORKER_H
 #define IC_PROXY_BGWORKER_H
 
+#include "port/atomics.h"
+
+/* flag (in SHM) for incidaing if peer listener bind/listen failed */
+extern pg_atomic_uint32 *ic_proxy_peer_listener_failed;
+
 extern bool ICProxyStartRule(Datum main_arg);
 extern void ICProxyMain(Datum main_arg);
+extern Size ICProxyShmemSize(void);
+extern void ICProxyShmemInit(void);
 
 #endif   /* IC_PROXY_BGWORKER_H */