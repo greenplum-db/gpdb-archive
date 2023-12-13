ecc8344c428622bd7b0d0943ba5de03f1b678915
Brent Doil <bdoil@vmware.com>
2023-12-12 20:49:00 -0500

    Produce stable explain_format test output
    This commit borrows two test helper functions from
    PG13 1001368 that replace changeable output details
    in the EXPLAIN output. As a result, remove the complex
    matchsubs in the explain_format.sql regression test.
    This results in stable test output across planner/orca and
    assert/non-assert builds.

--- src/test/regress/sql/explain_format.sql
+++ src/test/regress/sql/explain_format.sql
@@ -1,38 +1,52 @@
--- start_matchsubs
--- m/\(actual time=\d+\.\d+..\d+\.\d+ rows=\d+ loops=\d+\)/
--- s/\(actual time=\d+\.\d+..\d+\.\d+ rows=\d+ loops=\d+\)/(actual time=##.###..##.### rows=# loops=#)/
--- m/\(slice\d+\)    Executor memory: (\d+)\w bytes\./
--- s/Executor memory: (\d+)\w bytes\./Executor memory: (#####)K bytes./
--- m/\(slice\d+\)    Executor memory: (\d+)\w bytes avg x \d+ workers, \d+\w bytes max \(seg\d+\)\./
--- s/Executor memory: (\d+)\w bytes avg x \d+ workers, \d+\w bytes max \(seg\d+\)\./Executor memory: ####K bytes avg x #### workers, ####K bytes max (seg#)./
--- m/\(slice\d+\)    Executor memory: (\d+)\w bytes \(seg\d+\)\./
--- s/Executor memory: (\d+)\w bytes \(seg\d+\)\./Executor memory: ####K bytes (seg#)./
--- m/Work_mem: \d+\w bytes max\./
--- s/Work_mem: \d+\w bytes max\. */Work_mem: ###K bytes max./
--- m/Memory: \d+kB  Max Memory: \d+kB  Avg Memory: \d+kB \(3 segments\)/
--- s/Memory: \d+kB  Max Memory: \d+kB  Avg Memory: \d+kB \(3 segments\)/Memory: ###kB  Max Memory: ###kB  Avg Memory: ###kB \(3 segments\)/
--- m/work_mem: \d+kB  Segments: 3  Max: \d+kB \(segment \d+\)  Workfile: \(0 spilling\)/
--- s/work_mem: \d+kB  Segments: 3  Max: \d+kB \(segment \d+\)  Workfile: \(0 spilling\)/work_mem: ###kB  Segments: 3  Max: ###kB \(segment ##\)  Workfile: \(0 spilling\)/
--- m/Execution Time: \d+\.\d+ ms/
--- s/Execution Time: \d+\.\d+ ms/Execution Time: ##.### ms/
--- m/Planning Time: \d+\.\d+ ms/
--- s/Planning Time: \d+\.\d+ ms/Planning Time: ##.### ms/
--- m/cost=\d+\.\d+\.\.\d+\.\d+ rows=\d+ width=\d+/
--- s/\(cost=\d+\.\d+\.\.\d+\.\d+ rows=\d+ width=\d+\)/(cost=##.###..##.### rows=### width=###)/
--- m/Memory used:  \d+\w?B/
--- s/Memory used:  \d+\w?B/Memory used: ###B/
--- m/Memory Usage: \d+\w?B/
--- s/Memory Usage: \d+\w?B/Memory Usage: ###B/
--- m/Memory wanted:  \d+\w?kB/
--- s/Memory wanted:  \d+\w?kB/Memory wanted: ###kB/
--- m/Peak Memory Usage: \d+/
--- s/Peak Memory Usage: \d+/Peak Memory Usage: ###/
--- m/Buckets: \d+/
--- s/Buckets: \d+/Buckets: ###/
--- m/Batches: \d+/
--- s/Batches: \d+/Batches: ###/
--- end_matchsubs
---
+-- Tests EXPLAIN format output
+
+-- To produce stable regression test output, it's usually necessary to
+-- ignore details such as exact costs or row counts.  These filter
+-- functions replace changeable output details with fixed strings.
+create function explain_filter(text) returns setof text
+language plpgsql as
+$$
+declare
+    ln text;
+begin
+    for ln in execute $1
+    loop
+        -- Replace any numeric word with just 'N'
+        ln := regexp_replace(ln, '-?\m\d+\M', 'N', 'g');
+        -- In sort output, the above won't match units-suffixed numbers
+        ln := regexp_replace(ln, '\m\d+kB', 'NkB', 'g');
+        ln := regexp_replace(ln, '\m\d+K', 'NK', 'g');
+        -- Replace slice and segment numbers with 'N'
+        ln := regexp_replace(ln, '\mslice\d+', 'sliceN', 'g');
+        ln := regexp_replace(ln, '\mseg\d+', 'segN', 'g');
+        -- Ignore text-mode buffers output because it varies depending
+        -- on the system state
+        CONTINUE WHEN (ln ~ ' +Buffers: .*');
+        -- Ignore text-mode "Planning:" line because whether it's output
+        -- varies depending on the system state
+        CONTINUE WHEN (ln = 'Planning:');
+        return next ln;
+    end loop;
+end;
+$$;
+-- To produce valid JSON output, replace numbers with "0" or "0.0" not "N"
+create function explain_filter_to_json(text) returns jsonb
+language plpgsql as
+$$
+declare
+    data text := '';
+    ln text;
+begin
+    for ln in execute $1
+    loop
+        -- Replace any numeric word with just '0'
+        ln := regexp_replace(ln, '\m\d+\M', '0', 'g');
+        data := data || ln;
+    end loop;
+    return data::jsonb;
+end;
+$$;
+
 -- DEFAULT syntax
 CREATE TABLE apples(id int PRIMARY KEY, type text);
 INSERT INTO apples(id) SELECT generate_series(1, 100000);
@@ -40,14 +54,10 @@ CREATE TABLE box_locations(id int PRIMARY KEY, address text);
 CREATE TABLE boxes(id int PRIMARY KEY, apple_id int REFERENCES apples(id), location_id int REFERENCES box_locations(id));
 
 --- Check Explain Text format output
--- explain_processing_off
-EXPLAIN SELECT * from boxes LEFT JOIN apples ON apples.id = boxes.apple_id LEFT JOIN box_locations ON box_locations.id = boxes.location_id;
--- explain_processing_on
+select explain_filter('EXPLAIN SELECT * from boxes LEFT JOIN apples ON apples.id = boxes.apple_id LEFT JOIN box_locations ON box_locations.id = boxes.location_id;');
 
 --- Check Explain Analyze Text output that include the slices information
--- explain_processing_off
-EXPLAIN (ANALYZE) SELECT * from boxes LEFT JOIN apples ON apples.id = boxes.apple_id LEFT JOIN box_locations ON box_locations.id = boxes.location_id;
--- explain_processing_on
+select explain_filter('EXPLAIN (ANALYZE) SELECT * from boxes LEFT JOIN apples ON apples.id = boxes.apple_id LEFT JOIN box_locations ON box_locations.id = boxes.location_id;');
 
 -- Unaligned output format is better for the YAML / XML / JSON outputs.
 -- In aligned format, you have end-of-line markers at the end of each line,
@@ -56,75 +66,23 @@ EXPLAIN (ANALYZE) SELECT * from boxes LEFT JOIN apples ON apples.id = boxes.appl
 \a
 
 -- YAML Required replaces for costs and time changes
--- start_matchsubs
--- m/ Loops: \d+/
--- s/ Loops: \d+/ Loops: #/
--- m/ Cost: \d+\.\d+/
--- s/ Cost: \d+\.\d+/ Cost: ###.##/
--- m/ Rows: \d+/
--- s/ Rows: \d+/ Rows: #####/
--- m/ Plan Width: \d+/
--- s/ Plan Width: \d+/ Plan Width: ##/
--- m/ Time: \d+\.\d+/
--- s/ Time: \d+\.\d+/ Time: ##.###/
--- m/Execution Time: \d+\.\d+/
--- s/Execution Time: \d+\.\d+/Execution Time: ##.###/
--- m/Segments: \d+$/
--- s/Segments: \d+$/Segments: #/
--- m/Pivotal Optimizer \(GPORCA\) version \d+\.\d+\.\d+",?/
--- s/Pivotal Optimizer \(GPORCA\) version \d+\.\d+\.\d+",?/Pivotal Optimizer \(GPORCA\)"/
--- m/ Memory: \d+$/
--- s/ Memory: \d+$/ Memory: ###/
--- m/Maximum Memory Used: \d+/
--- s/Maximum Memory Used: \d+/Maximum Memory Used: ###/
--- m/Workers: \d+/
--- s/Workers: \d+/Workers: ##/
--- m/Average: \d+/
--- s/Average: \d+/Average: ##/
--- m/Total memory used across slices: \d+/
--- s/Total memory used across slices: \d+\s*/Total memory used across slices: ###/
--- m/Memory used: \d+/
--- s/Memory used: \d+/Memory used: ###/
--- end_matchsubs
--- start_matchignore
--- m/\s*optimizer:\s*"off"/
--- m/^\s+optimizer_jit\w*:/
--- end_matchignore
+
 -- Check Explain YAML output
-EXPLAIN (FORMAT YAML) SELECT * from boxes LEFT JOIN apples ON apples.id = boxes.apple_id LEFT JOIN box_locations ON box_locations.id = boxes.location_id;
+select explain_filter('EXPLAIN (FORMAT YAML) SELECT * from boxes LEFT JOIN apples ON apples.id = boxes.apple_id LEFT JOIN box_locations ON box_locations.id = boxes.location_id;');
 SET random_page_cost = 1;
 SET cpu_index_tuple_cost = 0.1;
-EXPLAIN (FORMAT YAML, VERBOSE) SELECT * from boxes;
--- ignore variable JIT gucs which can be showed when SETTINGS=ON
--- start_matchignore
--- m/^\s+jit\w*:/
--- end_matchignore
-EXPLAIN (FORMAT YAML, VERBOSE, SETTINGS ON) SELECT * from boxes;
+select explain_filter('EXPLAIN (FORMAT YAML, VERBOSE) SELECT * from boxes;');
+
+select explain_filter('EXPLAIN (FORMAT YAML, VERBOSE, SETTINGS ON) SELECT * from boxes;');
 
 --- Check Explain Analyze YAML output that include the slices information
--- explain_processing_off
-EXPLAIN (ANALYZE, FORMAT YAML) SELECT * from boxes LEFT JOIN apples ON apples.id = boxes.apple_id LEFT JOIN box_locations ON box_locations.id = boxes.location_id;
 
--- start_matchsubs
--- m/Executor Memory: \d+kB  Segments: 3  Max: \d+kB \(segment \d\)/
--- s/Executor Memory: \d+kB  Segments: 3  Max: \d+kB \(segment \d\)/Executor Memory: ###kB  Segments: 3  Max: ##kB (segment #)/
--- end_matchsubs
--- ignore the variable JIT gucs and "optimizer = 'off'" in Settings (unaligned mode + text format)
--- start_matchsubs
--- m/^Settings:.*/
--- s/,?\s*optimizer_jit\w*\s*=\s*[^,\n]+//g
--- m/^Settings:.*/
--- s/,?\s*jit\w*\s*=\s*[^,\n]+//g
--- m/^Settings:.*/
--- s/^Settings:[,\s]*/Settings: /
--- m/^Settings:.*/
--- s/,?\s*optimizer\w*\s*=\s*'off'//g
--- end_matchsubs
+select explain_filter('EXPLAIN (ANALYZE, FORMAT YAML) SELECT * from boxes LEFT JOIN apples ON apples.id = boxes.apple_id LEFT JOIN box_locations ON box_locations.id = boxes.location_id;');
+
 --- Check explain analyze sort information in verbose mode
-EXPLAIN (ANALYZE, VERBOSE) SELECT * from boxes ORDER BY apple_id;
+select explain_filter('EXPLAIN (ANALYZE, VERBOSE) SELECT * from boxes ORDER BY apple_id;');
 RESET random_page_cost;
 RESET cpu_index_tuple_cost;
--- explain_processing_on
 
 --
 -- Test a simple case with JSON and XML output, too.
@@ -133,31 +91,16 @@ RESET cpu_index_tuple_cost;
 -- XML, and YAML is in the formatting, after all.
 
 -- Check JSON format
---
--- start_matchsubs
--- m/Pivotal Optimizer \(GPORCA\) version \d+\.\d+\.\d+/
--- s/Pivotal Optimizer \(GPORCA\) version \d+\.\d+\.\d+/Pivotal Optimizer \(GPORCA\)/
--- end_matchsubs
--- explain_processing_off
-EXPLAIN (FORMAT JSON, COSTS OFF) SELECT * FROM generate_series(1, 10);
+select explain_filter_to_json('EXPLAIN (FORMAT JSON, COSTS OFF) SELECT * FROM generate_series(1, 10);');
 
-EXPLAIN (FORMAT XML, COSTS OFF) SELECT * FROM generate_series(1, 10);
+select explain_filter('EXPLAIN (FORMAT XML, COSTS OFF) SELECT * FROM generate_series(1, 10);');
 
 -- Test for an old bug in printing Sequence nodes in JSON/XML format
 -- (https://github.com/greenplum-db/gpdb/issues/9410)
 CREATE TABLE jsonexplaintest (i int4) PARTITION BY RANGE (i) (START(1) END(3) EVERY(1));
-EXPLAIN (FORMAT JSON, COSTS OFF) SELECT * FROM jsonexplaintest WHERE i = 2;
-
--- explain_processing_on
+select explain_filter_to_json('EXPLAIN (FORMAT JSON, COSTS OFF) SELECT * FROM jsonexplaintest WHERE i = 2;');
 
 -- Check Explain Text format output with jit enable
---
--- start_matchsubs
--- m/\(slice\d+\): Functions: \d+\.\d+\. Timing: \d+\.\d+ ms total\./
--- s/\(slice\d+\): Functions: \d+\.\d+\. Timing: \d+\.\d+ ms total\./\(slice###\): Functions: ##.###. Timing: ##.### ms total\./
--- m/\(slice\d+\): Functions: \d+\.\d+ avg x \d+ workers, \d+\.\d+ max \(seg\d+\)\. Timing: \d+\.\d+ ms avg x \d+ workers, \d+\.\d+ ms max \(seg\d+\)\./
--- s/\(slice\d+\): Functions: \d+\.\d+ avg x \d+ workers, \d+\.\d+ max \(seg\d+\)\. Timing: \d+\.\d+ ms avg x \d+ workers, \d+\.\d+ ms max \(seg\d+\)\./\(slice###\): Functions: ##.### avg x ### workers, ##.### max \(seg###\)\. Timing: ##.### ms avg x ### workers, ##.### ms max \(seg###\)\./
--- end_matchsubs
 CREATE TABLE jit_explain_output(c1 int);
 INSERT INTO jit_explain_output SELECT generate_series(1,100);
 
@@ -168,65 +111,19 @@ SET gp_explain_jit = on;
 -- ORCA GUCs to enable JIT
 set optimizer_jit_above_cost to 0;
 
--- explain_processing_off
-EXPLAIN SELECT * FROM jit_explain_output LIMIT 10;
--- explain_processing_on
+select explain_filter('EXPLAIN SELECT * FROM jit_explain_output LIMIT 10;');
 
--- Check explain anlyze text format output with jit enable 
--- explain_processing_off
-EXPLAIN (ANALYZE) SELECT * FROM jit_explain_output LIMIT 10;
--- explain_processing_on
+-- Check explain anlyze text format output with jit enable
+select explain_filter('EXPLAIN (ANALYZE) SELECT * FROM jit_explain_output LIMIT 10;');
 
 -- Check explain analyze json format output with jit enable
-
--- start_matchsubs
--- m/\"Actual Startup Time\": \d+\.\d+/
--- s/\"Actual Startup Time\": \d+\.\d+/\"Actual Startup Time\": ###/
--- m/\"Actual Total Time\": \d+\.\d+/
--- s/\"Actual Total Time\": \d+\.\d+/\"Actual Total Time\": ###/
--- m/\"Planning Time\": \d+\.\d+/
--- s/\"Planning Time\": \d+\.\d+/\"Planning Time\": ###/
--- m/\"Execution Time\": \d+\.\d+/
--- s/\"Execution Time\": \d+\.\d+/\"Execution Time\": ###/
--- m/\"Executor Memory\": \d+/
--- s/\"Executor Memory\": \d+/\"Executor Memory\": ###/
--- m/\"Average\": \d+/
--- s/\"Average\": \d+/\"Average\": ###/
--- m/\"Maximum Memory Used\": \d+/
--- s/\"Maximum Memory Used\": \d+/\"Maximum Memory Used\": ###/
--- m/\"slice\": \d+/
--- s/\"slice\": \d+/"slice": ###/
--- m/\"functions\": \d+\.\d+/
--- s/\"functions\": \d+\.\d+/\"functions\": ###/
--- m/\"Timing\": \d+\.\d+/
--- s/\"Timing\": \d+\.\d+/\"Timing": ###/
--- m/\"avg\": \d+\.\d+/
--- s/\"avg\": \d+\.\d+/\"avg\": ###/
--- m/\"nworker\": \d+/
--- s/\"nworker\": \d+/\"nworker\": ###/
--- m/\"max\": \d+\.\d+/
--- s/\"max\": \d+\.\d+/\"max\": ###/
--- m/\"segid\": \d+/
--- s/\"segid\": \d+/\"segid\": ###/
--- m/\"Memory used\": \d+/
--- s/\"Memory used\": \d+/\"Memory used\": ###/
--- end_matchsubs
-
--- explain_processing_off
-EXPLAIN (ANALYZE, FORMAT json) SELECT * FROM jit_explain_output LIMIT 10;
--- explain_processing_on
+select explain_filter_to_json('EXPLAIN (ANALYZE, FORMAT json) SELECT * FROM jit_explain_output LIMIT 10;');
 
 RESET jit;
 RESET jit_above_cost;
 RESET gp_explain_jit;
 RESET optimizer_jit_above_cost;
 
--- start_matchsubs
--- m/Extra Text: \(seg\d+\)   hash table\(s\): \d+; \d+ groups total in \d+ batches, \d+ spill partitions; disk usage: \d+KB; chain length \d+.\d+ avg, \d+ max; using \d+ of \d+ buckets; total \d+ expansions./
--- s/Extra Text: \(seg\d+\)   hash table\(s\): \d+; \d+ groups total in \d+ batches, \d+ spill partitions; disk usage: \d+KB; chain length \d+.\d+ avg, \d+ max; using \d+ of \d+ buckets; total \d+ expansions./Extra Text: (seg0)   hash table(s): ###; ### groups total in ### batches, ### spill partitions; disk usage: ###KB; chain length ###.## avg, ### max; using ## of ### buckets; total ### expansions./
--- m/Work_mem: \d+K bytes max, \d+K bytes wanted/
--- s/Work_mem: \d+K bytes max, \d+K bytes wanted/Work_mem: ###K bytes max, ###K bytes wanted/
--- end_matchsubs
 -- Greenplum hash table extra message
 CREATE TABLE test_src_tbl AS
 SELECT i % 10000 AS a, i % 10000 + 1 AS b FROM generate_series(1, 50000) i DISTRIBUTED BY (a);
@@ -239,97 +136,53 @@ SET statement_mem = '1000kB';
 -- Hashagg with spilling
 CREATE TABLE test_hashagg_spill AS
 SELECT a, COUNT(DISTINCT b) AS b FROM test_src_tbl GROUP BY a;
-WITH query_plan (et) AS
-(
-  SELECT test_util.get_explain_output(
-    'SELECT a, COUNT(DISTINCT b) AS b FROM test_src_tbl GROUP BY a',
-    false)
-)
-SELECT BTRIM(et) as explain_info FROM query_plan WHERE et like '%Extra Text%' limit 2;
+select explain_filter('EXPLAIN ANALYZE SELECT a, COUNT(DISTINCT b) AS b FROM test_src_tbl GROUP BY a;');
 
 -- Hashagg with grouping sets
 CREATE TABLE test_hashagg_groupingsets AS
 SELECT a, avg(b) AS b FROM test_src_tbl GROUP BY grouping sets ((a), (b));
 -- The planner generates multiple hash tables but ORCA uses Shared Scan.
-WITH query_plan (et) AS
-(
-  SELECT test_util.get_explain_output(
-    'SELECT a, avg(b) AS b FROM test_src_tbl GROUP BY grouping sets ((a), (b))',
-    false)
-)
-SELECT BTRIM(et) as explain_info FROM query_plan WHERE et like '%Extra Text%' limit 2;
+select explain_filter('EXPLAIN ANALYZE SELECT a, avg(b) AS b FROM test_src_tbl GROUP BY grouping sets ((a), (b));');
 
 RESET optimizer_enable_hashagg;
 RESET statement_mem;
 
 -- Check EXPLAIN format output with BUFFERS enabled
---
--- start_matchsubs
--- m{I/O Timings: read=\d*\.\d+ write=\d+\.\d+.*}
--- s{I/O Timings: read=\d*\.\d+ write=\d+\.\d+.*}{I/O Timings: read=##.### write=##.###}
--- m{I/O Timings: read=\d*\.\d+.*}
--- s{I/O Timings: read=\d*\.\d+.*}{I/O Timings: read=##.###.*}
--- m{I/O Read Time: \d*\.\d+}
--- s{I/O Read Time: \d*\.\d+}{I/O Read Time: ##.###}
--- m{I/O Write Time: \d*\.\d+}
--- s{I/O Write Time: \d*\.\d+}{I/O Write Time: ##.###}
--- m{Shared Hit Blocks: \d+}
--- s{Shared Hit Blocks: \d+}{Shared Hit Blocks: ###}
--- m{Shared Read Blocks: \d+}
--- s{Shared Read Blocks: \d+}{Shared Read Blocks: ###}
--- m{Shared Dirtied Blocks: \d+}
--- s{Shared Dirtied Blocks: \d+}{Shared Dirtied Blocks: ###}
--- m{Shared Written Blocks: \d+}
--- s{Shared Written Blocks: \d+}{Shared Written Blocks: ###}
--- m{Temp Read Blocks: \d+}
--- s{Temp Read Blocks: \d+}{Temp Read Blocks: ###}
--- m{Temp Written Blocks: \d+}
--- s{Temp Written Blocks: \d+}{Temp Written Blocks: ###}
--- m/Buffers: shared hit=\d+\s+/
--- s/Buffers: shared hit=\d+\s+Buffers: shared hit=###/
--- m/Buffers: shared hit=\d+ read=\d+\s+/
--- s/Buffers: shared hit=\d+ read=\d+\s+/Buffers: shared hit=### read=###/
--- m/Buffers: shared hit=\d+ read=\d+ dirtied=\d+\s+/
--- s/Buffers: shared hit=\d+ read=\d+ dirtied=\d+\s+/Buffers: shared hit=### read=### dirtied=###/
--- m/Buffers: shared hit=\d+ read=\d+ dirtied=\d+ written=\d+\s+/
--- s/Buffers: shared hit=\d+ read=\d+ dirtied=\d+ written=\d+\s+/Buffers: shared hit=### read=### dirtied=### written=###/
--- end_matchsubs
-
 -- Insert rows into a single segment
 SET track_io_timing = on;
 CREATE TABLE stat_io_timing(a, b) AS SELECT 0, i FROM generate_series(1, 10000) i DISTRIBUTED BY (a);
 ANALYZE stat_io_timing;
 
 -- explain_processing_off
-EXPLAIN (ANALYZE, BUFFERS, COSTS OFF)
+select explain_filter('EXPLAIN (ANALYZE, BUFFERS, COSTS OFF)
 SELECT a FROM stat_io_timing
-WHERE b BETWEEN 5 AND 9;
+WHERE b BETWEEN 5 AND 9;');
 
-EXPLAIN (ANALYZE, BUFFERS, COSTS OFF, SUMMARY OFF)
+select explain_filter('EXPLAIN (ANALYZE, BUFFERS, COSTS OFF, SUMMARY OFF)
 SELECT a FROM stat_io_timing
-WHERE b BETWEEN 5 AND 9;
+WHERE b BETWEEN 5 AND 9;');
 
-EXPLAIN (ANALYZE, BUFFERS, COSTS OFF)
-INSERT INTO stat_io_timing (SELECT * FROM stat_io_timing);
+select explain_filter('EXPLAIN (ANALYZE, BUFFERS, COSTS OFF)
+INSERT INTO stat_io_timing (SELECT * FROM stat_io_timing);');
 
-EXPLAIN (ANALYZE, BUFFERS, COSTS OFF, FORMAT JSON)
-INSERT INTO stat_io_timing (SELECT * FROM stat_io_timing);
+select explain_filter_to_json('EXPLAIN (ANALYZE, BUFFERS, COSTS OFF, FORMAT JSON)
+INSERT INTO stat_io_timing (SELECT * FROM stat_io_timing);');
 
-EXPLAIN (ANALYZE, BUFFERS, COSTS OFF, SUMMARY OFF)
-INSERT INTO stat_io_timing (SELECT * FROM stat_io_timing);
+select explain_filter('EXPLAIN (ANALYZE, BUFFERS, COSTS OFF, SUMMARY OFF)
+INSERT INTO stat_io_timing (SELECT * FROM stat_io_timing);');
 
-EXPLAIN (ANALYZE, BUFFERS, COSTS OFF, FORMAT JSON)
-SELECT b FROM stat_io_timing where b=50;
+select explain_filter_to_json('EXPLAIN (ANALYZE, BUFFERS, COSTS OFF, FORMAT JSON)
+SELECT b FROM stat_io_timing where b=50;');
 
 CREATE INDEX stat_io_timing_idx ON stat_io_timing (b);
-EXPLAIN (ANALYZE, BUFFERS, COSTS OFF)
-SELECT b FROM stat_io_timing where b=50;
+select explain_filter('EXPLAIN (ANALYZE, BUFFERS, COSTS OFF)
+SELECT b FROM stat_io_timing where b=50;');
 
-EXPLAIN (ANALYZE, BUFFERS, COSTS OFF, FORMAT JSON)
-SELECT b FROM stat_io_timing where b=50;
+select explain_filter_to_json('EXPLAIN (ANALYZE, BUFFERS, COSTS OFF, FORMAT JSON)
+SELECT b FROM stat_io_timing where b=50;');
 
-EXPLAIN (ANALYZE, BUFFERS, COSTS OFF)
-SELECT s1.b FROM stat_io_timing s1 join stat_io_timing s2 on s1.b=s2.b where s1.a=50;
+select explain_filter('EXPLAIN (ANALYZE, BUFFERS, COSTS OFF)
+SELECT s1.b FROM stat_io_timing s1 join stat_io_timing s2 on s1.b=s2.b where s1.a=50;');
 
 -- Test Bitmap Heap Scan block accounting
 SET enable_seqscan = 0;
@@ -340,10 +193,8 @@ SET optimizer_enable_bitmapscan = 1;
 CREATE INDEX stat_io_timing_brin_idx ON stat_io_timing USING brin (b);
 VACUUM ANALYZE stat_io_timing;
 
-EXPLAIN (ANALYZE, BUFFERS, COSTS OFF)
-SELECT * FROM stat_io_timing WHERE b = 1;
-
--- explain_processing_on
+select explain_filter('EXPLAIN (ANALYZE, BUFFERS, COSTS OFF)
+SELECT * FROM stat_io_timing WHERE b = 1;');
 
 RESET track_io_timing;
 RESET enable_seqscan;
