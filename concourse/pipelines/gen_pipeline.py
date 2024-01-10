dac3bd5a6b5602fa10582b0be46505613fe1a367
Anusha Shakarad <shakarada@vmware.com>
2024-01-10 11:42:23 +0530

    Change compile platform to oel9 for el9 platforms as llvm 15 is not supported in rocky9
    Authored-by: Anusha Shakarad <shakarada@vmware.com>

--- concourse/pipelines/gen_pipeline.py
+++ concourse/pipelines/gen_pipeline.py
@@ -115,9 +115,9 @@ def create_pipeline(args, git_remote, git_branch):
         "rhel8" : "rocky8",
         "rocky8": "rocky8",
         "oel8" : "rocky8",
-        "rhel9" : "rocky9",
-        "rocky9": "rocky9",
-        "oel9" : "rocky9"
+        "rhel9" : "rhel9",
+        "rocky9": "rhel9",
+        "oel9" : "rhel9"
     }
     dist = {
         "rhel8" : "el8",
